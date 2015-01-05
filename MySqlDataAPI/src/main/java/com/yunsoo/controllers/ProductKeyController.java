c329fdf2910ca13eb972d4d8bd8c5d6bedd121a8
Lijian <loye.qiu@live.com>
2015-01-05 16:14:48 +0800

    initial commit

---
+++ MySqlDataAPI/src/main/java/com/yunsoo/controllers/ProductKeyController.java
@@ -0,0 +1,10 @@
+package com.yunsoo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductKeyController {
+
+}
