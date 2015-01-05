c72b6c9f5a1b7ed7a3d1d1cbfeaf98bec4e17731
Lijian <loye.qiu@live.com>
2015-01-05 16:44:32 +0800

    initial commit

---
+++ MySqlDataAPI/src/main/java/com/yunsoo/ProductKey.java
@@ -0,0 +1,21 @@
+package com.yunsoo;
+
+public class ProductKey {
+    private String key;
+    private String productId;
+
+    public ProductKey(String key, String productId) {
+        this.key = key;
+        this.productId = productId;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+
+}
--- MySqlDataAPI/src/main/java/com/yunsoo/controllers/ProductKeyController.java
+++ MySqlDataAPI/src/main/java/com/yunsoo/controllers/ProductKeyController.java
@@ -1,10 +1,16 @@
 package com.yunsoo.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import com.yunsoo.ProductKey;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class ProductKeyController {
 
+    @RequestMapping(value = "/productkey", method = RequestMethod.GET)
+    public ProductKey newProductKey() {
+        return new ProductKey("EH6MhZATukqeKRADNOiBng", "12345");
+    }
 }
