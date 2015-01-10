b686f2d6d14078f56626035d808f07e5c3a4ae39
Zhe Zhang <nakayamaqs@hotmail.com>
2015-01-10 19:25:13 +0800

    Update Product.java

--- YunsooDataService/src/main/java/com/yunsoo/model/Product.java
+++ YunsooDataService/src/main/java/com/yunsoo/model/Product.java
@@ -9,66 +9,68 @@ import javax.persistence.*;
 @Table(name = "product")
 public class Product implements Serializable {
 
-	@Id
-	@GeneratedValue
-	@Column(name = "id")
-	private int Id;
-	@Column(name = "BASE_PRODUCT_ID")
-	private int baseProductId;
-	@Column(name = "PRODUCT_STATUS_ID")
-	private int productStatusId;
-	@Column(name = "MANUFACTURING_DATE")
-	private Date manufacturingDate;
-	@Column(name = "CREATED_DATETIME")
-	private Date createdDateTime;
-	private BaseProduct baseProduct;
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private int Id;
+    @Column(name = "BASE_PRODUCT_ID")
+    private int baseProductId;
+    @Column(name = "PRODUCT_STATUS_ID")
+    private int productStatusId;
+    @Column(name = "MANUFACTURING_DATE")
+    private Date manufacturingDate;
+    @Column(name = "CREATED_DATETIME")
+    private Date createdDateTime;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "BASE_PRODUCT_ID", insertable = false, updatable = false, nullable = false)
+    private BaseProduct baseProduct;
 
-	public int getId() {
-		return Id;
-	}
+    public int getId() {
+        return Id;
+    }
 
-	public void setId(int id) {
-		this.Id = id;
-	}
+    public void setId(int id) {
+        this.Id = id;
+    }
 
-	public int getBaseProducctId() {
-		return baseProductId;
-	}
+    public int getBaseProducctId() {
+        return baseProductId;
+    }
 
-	public void setBaseProductId(int baseProductId) {
-		this.baseProductId = baseProductId;
-	}
+    public void setBaseProductId(int baseProductId) {
+        this.baseProductId = baseProductId;
+    }
 
-	public int getProductStatusId() {
-		return productStatusId;
-	}
+    public int getProductStatusId() {
+        return productStatusId;
+    }
 
-	public void setProductStatusId(int productStatusId) {
-		this.productStatusId = productStatusId;
-	}
+    public void setProductStatusId(int productStatusId) {
+        this.productStatusId = productStatusId;
+    }
 
-	public Date getManufacturingDate() {
-		return manufacturingDate;
-	}
+    public Date getManufacturingDate() {
+        return manufacturingDate;
+    }
 
-	public void setManufacturingDate(Date manufacturingDate) {
-		this.manufacturingDate = manufacturingDate;
-	}
+    public void setManufacturingDate(Date manufacturingDate) {
+        this.manufacturingDate = manufacturingDate;
+    }
 
-	public Date getCreatedDateTime() {
-		return createdDateTime;
-	}
+    public Date getCreatedDateTime() {
+        return createdDateTime;
+    }
 
-	public void setCreatedDateTime(Date createdDate) {
-		this.createdDateTime = createdDate;
-	}
+    public void setCreatedDateTime(Date createdDate) {
+        this.createdDateTime = createdDate;
+    }
 
-	public BaseProduct getBaseProduct() {
-		return baseProduct;
-	}
+    public BaseProduct getBaseProduct() {
+        return baseProduct;
+    }
 
-	public void setBaseProduct(BaseProduct baseProduct) {
-		this.baseProduct = baseProduct;
-	}
+    public void setBaseProduct(BaseProduct baseProduct) {
+        this.baseProduct = baseProduct;
+    }
 
 }
