efcf6e1afacb82abe3fbf8145800de60abbd0c4e
Lijian <loye.qiu@gmail.com>
2015-02-02 10:45:06 +0800

    add Services

---
+++ dataService/src/main/java/com/yunsoo/service/Impl/ProductKeyBatchServiceImpl.java
@@ -0,0 +1,12 @@
+package com.yunsoo.service.Impl;
+
+import com.yunsoo.service.ProductKeyBatchService;
+
+/**
+ * Created by:   Lijian
+ * Created on:   2015/2/1
+ * Descriptions:
+ */
+public class ProductKeyBatchServiceImpl implements ProductKeyBatchService {
+
+}
