1be843c4507f7aee03d8976226d33d04c1fd59c3
Jerry <chenruchang2002@163.com>
2015-02-15 20:08:50 +0800

    Fix js issue and update to the new css

--- AdminLTE/src/app/js/pages/msg/msg.js
+++ AdminLTE/src/app/js/pages/msg/msg.js
@@ -3,6 +3,8 @@
 
     app.filter('startFrom', function () {
         return function (input, start) {
+            if (!input || !input.length) { return; }
+
             return input.slice(start - 1);
         };
     });
