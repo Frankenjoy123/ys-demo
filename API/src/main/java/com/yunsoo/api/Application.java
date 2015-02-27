fbe1711edc5137f6fbfadb3a3361ce629e8b9df5
Zhe Zhang <nakayamaqs@hotmail.com>
2015-02-27 15:10:05 +0800

    run as web app

--- API/src/main/java/com/yunsoo/api/Application.java
+++ API/src/main/java/com/yunsoo/api/Application.java
@@ -1,7 +1,9 @@
 package com.yunsoo.api;
 
 import com.yunsoo.api.object.Page;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,6 +19,9 @@ public class Application {
         System.out.println("About:   " + page.getAbout());
         System.out.println("Phone:   " + page.getPhone());
         System.out.println("Website: " + page.getWebsite());
+
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        System.out.println("Run API by Spring Boot:");
     }
 
 
