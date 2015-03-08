1cf63d6bc0e48f44dbabfaa6cba1d764cb48ee4a
gianfrancochn <gianfranco@163.com>
2015-03-09 00:47:54 +0800

    account related

---
+++ dataService/src/main/java/com/yunsoo/util/ConvertHelper.java
@@ -0,0 +1,18 @@
+package com.yunsoo.util;
+
+/**
+ * Created by admin on 2015/3/8.
+ */
+public class ConvertHelper {
+    public static String joinLongArray(String split, long[] input) {
+        String output = "";
+        for (int i=0;i<input.length;i++){
+            if (i<input.length - 1) {
+                output += Long.toString(input[i]) + split;
+            } else {
+                output += Long.toString(input[i]);
+            }
+        }
+        return output;
+    }
+}
