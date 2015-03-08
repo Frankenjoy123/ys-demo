ce4d4413581ac75a57a1e9e0925b08100b20098e
Zhe Zhang <nakayamaqs@hotmail.com>
2015-03-08 16:16:46 +0800

    update applicatoin.java

--- dataAPI/src/main/java/com/yunsoo/dataapi/Application.java
+++ dataAPI/src/main/java/com/yunsoo/dataapi/Application.java
@@ -14,24 +14,12 @@ import java.util.Arrays;
 public class Application {
 
     public static void main(String[] args) {
-//        ApplicationContext context =
-//                new AnnotationConfigApplicationContext(Application.class);
-
-        //ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml.bc");
         ApplicationContext context = SpringApplication.run(Application.class, args);
 
         //set throw exception if no handler mapping
         Object dispatcherServlet = context.getBean("dispatcherServlet");
         if (dispatcherServlet != null && dispatcherServlet instanceof DispatcherServlet) {
             ((DispatcherServlet) dispatcherServlet).setThrowExceptionIfNoHandlerFound(true);
-        }
-
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-        String[] beanNames = context.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
         }
 
         System.out.println("Run DataAPI by Spring Boot. Successfully started...");
