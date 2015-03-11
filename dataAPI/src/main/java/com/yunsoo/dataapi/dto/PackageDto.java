b33bb1850a12190b7701f9c4563cdc989ee91f65
qiyong <yqy0902@gmail.com>
2015-03-11 16:29:06 +0800

    finished package api implementation. TODO: remove duplicated keys in batch operation TODO: Web UI part.

---
+++ dataAPI/src/main/java/com/yunsoo/dataapi/dto/PackageDto.java
@@ -0,0 +1,80 @@
+package com.yunsoo.dataapi.dto;
+
+import com.yunsoo.service.contract.PackageContract;
+import com.yunsoo.service.contract.Product;
+import org.joda.time.DateTime;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Hope on 2015/3/7.
+ */
+public class PackageDto {
+
+    private String key;
+    private int productCount;
+    private int packageCount;
+    private List<PackageDto> subPackages;
+    private long operator; // Will join to User table to get the user info
+
+    public PackageDto() {
+    }
+
+    public PackageDto(PackageContract contract) {
+        this.key = contract.getKey();
+        this.productCount = contract.getProductCount();
+        this.packageCount = contract.getPackageCount();
+        this.operator = contract.getOperator();
+        List<PackageContract> subContracts = contract.getSubPackages();
+
+        if (subContracts != null) {
+            List<PackageDto> dtos = new ArrayList<>();
+            for (PackageContract subContract : subContracts) {
+                dtos.add(new PackageDto(subContract));
+            }
+            this.subPackages = dtos;
+        }
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+
+    public int getProductCount() {
+        return productCount;
+    }
+
+    public void setProductCount(int productCount) {
+        this.productCount = productCount;
+    }
+
+    public int getPackageCount() {
+        return packageCount;
+    }
+
+    public void setPackageCount(int packageCount) {
+        this.packageCount = packageCount;
+    }
+
+    public List<PackageDto> getSubPackages() {
+        return subPackages;
+    }
+
+    public void setSubPackages(List<PackageDto> subPackages) {
+        this.subPackages = subPackages;
+    }
+
+    public long getOperator() {
+        return operator;
+    }
+
+    public void setOperator(long operator) {
+        this.operator = operator;
+    }
+}
