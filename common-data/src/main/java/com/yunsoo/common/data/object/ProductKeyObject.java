2f4d9313057d9f9d5a9374608fa3da470638ec54
Lijian <loye.qiu@gmail.com>
2015-03-16 11:39:30 +0800

    add common-data

---
+++ common-data/src/main/java/com/yunsoo/common/data/object/ProductKeyObject.java
@@ -0,0 +1,90 @@
+package com.yunsoo.common.data.object;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.yunsoo.common.DateTimeJsonDeserializer;
+import org.joda.time.DateTime;
+
+import java.util.Set;
+
+/**
+ * Created by:   Lijian
+ * Created on:   2015/3/16
+ * Descriptions:
+ */
+public class ProductKeyObject {
+
+    private String productKey;
+    private int productKeyTypeId;
+    private boolean productKeyDisabled;
+    private boolean primary;
+    private int productKeyBatchId;
+    private String primaryProductKey;
+    private Set<String> productKeySet;
+    @JsonDeserialize(using = DateTimeJsonDeserializer.class)
+    private DateTime createdDateTime;
+
+    public String getProductKey() {
+        return productKey;
+    }
+
+    public void setProductKey(String productKey) {
+        this.productKey = productKey;
+    }
+
+    public int getProductKeyTypeId() {
+        return productKeyTypeId;
+    }
+
+    public void setProductKeyTypeId(int productKeyTypeId) {
+        this.productKeyTypeId = productKeyTypeId;
+    }
+
+    public boolean isProductKeyDisabled() {
+        return productKeyDisabled;
+    }
+
+    public void setProductKeyDisabled(boolean productKeyDisabled) {
+        this.productKeyDisabled = productKeyDisabled;
+    }
+
+    public boolean isPrimary() {
+        return primary;
+    }
+
+    public void setPrimary(boolean primary) {
+        this.primary = primary;
+    }
+
+    public int getProductKeyBatchId() {
+        return productKeyBatchId;
+    }
+
+    public void setProductKeyBatchId(int productKeyBatchId) {
+        this.productKeyBatchId = productKeyBatchId;
+    }
+
+    public String getPrimaryProductKey() {
+        return primaryProductKey;
+    }
+
+    public void setPrimaryProductKey(String primaryProductKey) {
+        this.primaryProductKey = primaryProductKey;
+    }
+
+    public Set<String> getProductKeySet() {
+        return productKeySet;
+    }
+
+    public void setProductKeySet(Set<String> productKeySet) {
+        this.productKeySet = productKeySet;
+    }
+
+    public DateTime getCreatedDateTime() {
+        return createdDateTime;
+    }
+
+    public void setCreatedDateTime(DateTime createdDateTime) {
+        this.createdDateTime = createdDateTime;
+    }
+
+}
---
+++ common-data/src/main/java/com/yunsoo/common/data/object/ProductObject.java
@@ -0,0 +1,66 @@
+package com.yunsoo.common.data.object;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.yunsoo.common.DateTimeJsonDeserializer;
+import com.yunsoo.common.DateTimeJsonSerializer;
+import org.joda.time.DateTime;
+
+/**
+ * Created by:   Lijian
+ * Created on:   2015/3/16
+ * Descriptions:
+ */
+public class ProductObject {
+
+    private String productKey;
+    private int productBaseId;
+    private int productStatusId;
+    @JsonSerialize(using = DateTimeJsonSerializer.class)
+    @JsonDeserialize(using = DateTimeJsonDeserializer.class)
+    private DateTime manufacturingDateTime;
+    @JsonSerialize(using = DateTimeJsonSerializer.class)
+    @JsonDeserialize(using = DateTimeJsonDeserializer.class)
+    private DateTime createdDateTime;
+
+    public String getProductKey() {
+        return productKey;
+    }
+
+    public void setProductKey(String productKey) {
+        this.productKey = productKey;
+    }
+
+    public int getProductBaseId() {
+        return productBaseId;
+    }
+
+    public void setProductBaseId(int productBaseId) {
+        this.productBaseId = productBaseId;
+    }
+
+    public int getProductStatusId() {
+        return productStatusId;
+    }
+
+    public void setProductStatusId(int productStatusId) {
+        this.productStatusId = productStatusId;
+    }
+
+    public DateTime getManufacturingDateTime() {
+        return manufacturingDateTime;
+    }
+
+    public void setManufacturingDateTime(DateTime manufacturingDateTime) {
+        this.manufacturingDateTime = manufacturingDateTime;
+    }
+
+    public DateTime getCreatedDateTime() {
+        return createdDateTime;
+    }
+
+    public void setCreatedDateTime(DateTime createdDateTime) {
+        this.createdDateTime = createdDateTime;
+    }
+
+}
