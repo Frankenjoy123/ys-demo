c6458171a9e99391c4646775a9cbed4731fd4a53
Jerry <chenruchang2002@163.com>
2015-03-19 16:45:21 +0800

    update logistics page to call the api

--- web-enterprise/src/app/js/pages/logistics/logistics.js
+++ web-enterprise/src/app/js/pages/logistics/logistics.js
@@ -1,25 +1,32 @@
-(function(){
-    var app = angular.module("logistics",["interceptor"]);
+(function () {
+    var app = angular.module("logistics", ["interceptor"]);
 
-    app.factory("logisticsService", ["$http", function($http){
+    app.factory("logisticsService", ["$http", function ($http) {
         return {
-            getInfo: function(fnSuccess, fnError){
-                $http.get("mock/logistics.json")
-                    .success(function(data){
+            getInfo: function (productKey, fnSuccess, fnError) {
+                $http.get("/api/logistics/" + productKey)
+                    .success(function (data) {
                         fnSuccess(data);
+                    }).error(function (data, state) {
+                        fnSuccess();
                     });
             }
         };
     }]);
 
-    app.controller("logisticsCtrl", ["$scope", "logisticsService", function($scope, logisticsService){
-        logisticsService.getInfo(function(data){
-            $scope.data = data;
-        });
+    app.controller("logisticsCtrl", ["$scope", "logisticsService", function ($scope, logisticsService) {
+
+        $scope.productKey = "";
 
         $scope.getDateString = function (value) {
             var date = new Date(value);
             return new DateTime(date).toString('yyyy-MM-dd HH:mm:ss');
         };
+
+        $scope.productKeyClick = function () {
+            logisticsService.getInfo($scope.productKey, function (data) {
+                $scope.data = data;
+            });
+        }
     }]);
 })();
