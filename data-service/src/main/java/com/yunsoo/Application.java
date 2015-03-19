9a4038fe5fa6de07758fbfe8021fae474dfb8f00
Lijian <loye.qiu@gmail.com>
2015-03-19 14:29:11 +0800

    add main method to Application

---
+++ data-service/src/main/java/com/yunsoo/Application.java
@@ -0,0 +1,13 @@
+package com.yunsoo;
+
+/**
+ * Created by:   Lijian
+ * Created on:   2015/3/19
+ * Descriptions: It's added only for avoid bootRepackage issue during building.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Do no thing in data-service main method");
+
+    }
+}
