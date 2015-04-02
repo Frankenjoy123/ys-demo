72d5a9afbdf94eff32d99b3b8a7571dac45adbbc
Lijian <loye.qiu@gmail.com>
2015-04-02 11:41:12 +0800

    add processor

---
+++ processor/src/main/java/com/yunsoo/processor/AmazonSqsConfig.java
@@ -0,0 +1,22 @@
+package com.yunsoo.processor;
+
+import com.amazonaws.services.sqs.AmazonSQS;
+import org.springframework.cloud.aws.core.env.ResourceIdResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by:   Lijian
+ * Created on:   2015/3/31
+ * Descriptions:
+ */
+@Configuration
+public class AmazonSqsConfig {
+
+    @Bean
+    public QueueMessagingTemplate queueMessagingTemplate(AmazonSQS amazonSqs, ResourceIdResolver resourceIdResolver) {
+        return new QueueMessagingTemplate(amazonSqs, resourceIdResolver);
+    }
+
+}
