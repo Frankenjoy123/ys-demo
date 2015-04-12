f93da9a33139df5865b9aa5df148c888941f2af9
Lijian <loye.qiu@gmail.com>
2015-04-12 22:55:59 +0800

    add permission repositories

---
+++ data-service/src/main/java/com/yunsoo/data/service/repository/AccountPermissionRepository.java
@@ -0,0 +1,13 @@
+package com.yunsoo.data.service.repository;
+
+import com.yunsoo.data.service.entity.AccountPermissionEntity;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by:   Lijian
+ * Created on:   2015/4/12
+ * Descriptions:
+ */
+public interface AccountPermissionRepository extends CrudRepository<AccountPermissionEntity, Long> {
+}
+
