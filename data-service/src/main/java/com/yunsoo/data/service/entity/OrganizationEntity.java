e4e33976ad23c7294a58e102c901ef32c5d351c3
Lijian <loye.qiu@gmail.com>
2015-04-17 10:58:11 +0800

    account & org

--- data-service/src/main/java/com/yunsoo/data/service/entity/OrganizationEntity.java
+++ data-service/src/main/java/com/yunsoo/data/service/entity/OrganizationEntity.java
@@ -1,7 +1,8 @@
 package com.yunsoo.data.service.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
 
 /**
  * Created by:   Lijian
@@ -11,12 +12,85 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "organization")
 public class OrganizationEntity {
+
+    @Id
+    @GeneratedValue(generator = "idGenerator")
+    @GenericGenerator(name = "idGenerator", strategy = "com.yunsoo.data.service.util.IdGenerator")
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "name")
     private String name;
-    private String description;
-    private String imageUrl;
-    private String detail;
+
+    @Column(name = "type")
     private int type;
+
+    @Column(name = "status")
     private int status;
 
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "imageUri")
+    private String imageUri;
+
+    @Column(name = "details")
+    private String details;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUri() {
+        return imageUri;
+    }
+
+    public void setImageUri(String imageUri) {
+        this.imageUri = imageUri;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
 }
