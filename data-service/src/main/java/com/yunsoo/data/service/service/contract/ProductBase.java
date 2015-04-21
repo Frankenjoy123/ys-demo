92002cc6d1ac91c50bf785089063db8fdffe2d53
Lijian <loye.qiu@gmail.com>
2015-04-21 12:06:29 +0800

    fix issue of ProductBase

--- data-service/src/main/java/com/yunsoo/data/service/service/contract/ProductBase.java
+++ data-service/src/main/java/com/yunsoo/data/service/service/contract/ProductBase.java
@@ -21,7 +21,7 @@ public class ProductBase {
 
     private String id;
     private Integer categoryId;
-    private Long orgId;
+    private String orgId;
     private String barcode;
     private String name;
     private String description;
@@ -54,11 +54,11 @@ public class ProductBase {
         this.categoryId = baseProductId;
     }
 
-    public void setOrgId(Long orgId) {
+    public void setOrgId(String orgId) {
         this.orgId = orgId;
     }
 
-    public Long getOrgId() {
+    public String getOrgId() {
         return orgId;
     }
 
