9241d75a6dc9db3ffc094240deffeb72d53b6ee4
Lijian <loye.qiu@gmail.com>
2015-04-24 11:46:31 +0800

    add rememberMe on login page

--- web-enterprise/src/app/js/pages/login.js
+++ web-enterprise/src/app/js/pages/login.js
@@ -3,50 +3,60 @@
 
     app.factory("loginService", ["$http", function ($http) {
         return {
-            login: function (org_id, username, password, fnSuccess, fnError) {
+            login: function (orgId, identifier, password, fnSuccess, fnError) {
                 $http.post("/api/auth/login", {
-                    org_id: org_id,
-                    identifier: username,
+                    org_id: orgId,
+                    identifier: identifier,
                     password: password
                 }).success(function (data) {
                     fnSuccess(data);
                 }).error(function (data) {
-                    fnError();
+                    fnError(data);
                 });
+            },
+            loginForm: function (loginForm) {
+                if (loginForm) {
+                    localStorage.loginForm = JSON.stringify(loginForm);
+                } else {
+                    return localStorage.loginForm ? JSON.parse(localStorage.loginForm) : null;
+                }
             }
         };
     }]);
 
     app.controller("loginController", ["$scope", "$timeout", "loginService",
         function ($scope, $timeout, loginService) {
-            $scope.username = "";
-            $scope.password = "";
-            $scope.org_id = "";
+            $scope.loginForm = loginService.loginForm();
+            $scope.loginForm || ($scope.loginForm = {
+                orgId: "",
+                identifier: "",
+                password: "",
+                rememberMe: false
+            });
+
             $scope.alertMsgs = [];
             $scope.login = function () {
-                if (!$scope.org_id) {
+                var loginForm = $scope.loginForm;
+                if (!loginForm.orgId) {
                     $scope.addAlertMsg("组织ID不能为空", "danger");
                     return;
                 }
-                if (!$scope.username) {
+                if (!loginForm.identifier) {
                     $scope.addAlertMsg("用户名不能为空", "danger");
                     return;
                 }
-                if (!$scope.password) {
+                if (!loginForm.password) {
                     $scope.addAlertMsg("密码不能为空", "danger");
                     return;
                 }
-                loginService.login($scope.org_id, $scope.username, $scope.password, function (data) {
+                loginService.login($scope.org_id, $scope.username, $scope.password, function(data){
                     if (!data || !data.access_token || !data.access_token.token) {
                         $scope.addAlertMsg("登陆失败，请再次尝试", "danger");
                         return;
                     }
-                    $.cookie(YUNSOO_CONFIG.AUTH_COOKIE_NAME, data.access_token.token, {
-                        expires: data.access_token.expires_in / (60 * 60 * 24),
-                        path: '/'
-                    });
+                    $.cookie(YUNSOO_CONFIG.AUTH_COOKIE_NAME, data.access_token.token, {expires: data.access_token.expires_in / (60 * 60 * 24), path: '/'});
                     window.location.href = "index.html";
-                }, function () {
+                },function(){
                     $scope.addAlertMsg("登陆失败，请再次尝试", "danger");
                 });
             };
