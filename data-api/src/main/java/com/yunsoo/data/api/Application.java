bd9934e4905e90415448637ef5791b5cbd13fc52
Zhe Zhang <nakayamaqs@hotmail.com>
2015-04-24 19:46:12 +0800

    try fix JPA

--- data-api/src/main/java/com/yunsoo/data/api/Application.java
+++ data-api/src/main/java/com/yunsoo/data/api/Application.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.*;
 import org.springframework.context.ApplicationContext;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @ComponentScan(basePackages = "com.yunsoo")
 @SpringBootApplication
+@EnableJpaRepositories
 public class Application {
 
     public static void main(String[] args) {
