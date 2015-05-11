6e7ff4da85d411eb22dde5ccbd2883b794c302d6
Jerry <chenruchang2002@163.com>
2015-05-11 10:58:02 +0800

    Add exception handle for logistics

--- api/src/main/java/com/yunsoo/api/domain/LogisticsDomain.java
+++ api/src/main/java/com/yunsoo/api/domain/LogisticsDomain.java
@@ -4,6 +4,8 @@ import com.yunsoo.api.dto.LogisticsPath;
 import com.yunsoo.common.data.object.*;
 import com.yunsoo.common.web.client.RestClient;
 import com.yunsoo.common.web.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,16 +25,17 @@ public class LogisticsDomain {
     @Autowired
     private RestClient dataAPIClient;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogisticsDomain.class);
+
     public List<LogisticsPath> getLogisticsPathsOrderByStartDate(String key) {
         List<LogisticsPath> logisticsPaths = new ArrayList<LogisticsPath>();
 
         LogisticsPathObject[] logisticsPathObjects = dataAPIClient.get("logisticspath/key/{key}", LogisticsPathObject[].class, key);
-        if(logisticsPathObjects == null || logisticsPathObjects.length == 0)
+        if (logisticsPathObjects == null || logisticsPathObjects.length == 0)
             throw new NotFoundException("Logistics path not found key=" + key);
 
         List<LogisticsPathObject> logisticsPathObjectsList = Arrays.asList(logisticsPathObjects);
-        for(LogisticsPathObject path : logisticsPathObjectsList)
-        {
+        for (LogisticsPathObject path : logisticsPathObjectsList) {
             LogisticsPath logisticsPath = new LogisticsPath();
             logisticsPath.setDesc(path.getDesc());
             logisticsPath.setEndDate(path.getEndDate());
@@ -40,35 +43,54 @@ public class LogisticsDomain {
             logisticsPath.setProductKey(path.getProductKey());
             logisticsPath.setStartDate(path.getStartDate());
 
-            if(path.getAction_id() != null) {
-                LogisticsCheckActionObject actionObject = dataAPIClient.get("logisticscheckaction/{id}", LogisticsCheckActionObject.class, path.getAction_id());
-                logisticsPath.setActionObject(actionObject);
-            }
-            else {
+            if (path.getAction_id() != null) {
+                try {
+                    LogisticsCheckActionObject actionObject = dataAPIClient.get("logisticscheckaction/{id}", LogisticsCheckActionObject.class, path.getAction_id());
+                    logisticsPath.setActionObject(actionObject);
+                } catch (Exception ex) {
+                    logisticsPath.setActionObject(null);
+                    LOGGER.error("getLogisticsPathsOrderByStartDate error", ex);
+                }
+            } else {
                 logisticsPath.setActionObject(null);
             }
 
-            if(path.getStartCheckPoint() != null) {
-                LogisticsCheckPointObject startPointObject = dataAPIClient.get("logisticscheckpoint/{id}", LogisticsCheckPointObject.class, path.getStartCheckPoint());
-                logisticsPath.setStartCheckPointObject(startPointObject);
-
-                if(startPointObject != null) {
-                    OrganizationObject startPointOrgObject = dataAPIClient.get("organization/{id}", OrganizationObject.class, startPointObject.getOrgId());
-                    logisticsPath.setStartCheckPointOrgObject(startPointOrgObject);
+            if (path.getStartCheckPoint() != null) {
+                LogisticsCheckPointObject startPointObject = null;
+                try {
+                    startPointObject = dataAPIClient.get("logisticscheckpoint/{id}", LogisticsCheckPointObject.class, path.getStartCheckPoint());
+                    logisticsPath.setStartCheckPointObject(startPointObject);
+                } catch (Exception ex) {
+                    logisticsPath.setStartCheckPointObject(null);
+                    LOGGER.error("getLogisticsPathsOrderByStartDate error", ex);
                 }
-                else{
+
+                if (startPointObject != null) {
+                    try {
+                        OrganizationObject startPointOrgObject = dataAPIClient.get("organization/{id}", OrganizationObject.class, startPointObject.getOrgId());
+                        logisticsPath.setStartCheckPointOrgObject(startPointOrgObject);
+                    }
+                    catch (Exception ex) {
+                        logisticsPath.setStartCheckPointOrgObject(null);
+                        LOGGER.error("getLogisticsPathsOrderByStartDate error", ex);
+                    }
+                } else {
                     logisticsPath.setStartCheckPointOrgObject(null);
                 }
-            }
-            else {
+            } else {
                 logisticsPath.setStartCheckPointObject(null);
             }
 
-            if(path.getEndCheckPoint() != null) {
-                LogisticsCheckPointObject endPointObject = dataAPIClient.get("logisticscheckpoint/{id}", LogisticsCheckPointObject.class, path.getEndCheckPoint());
-                logisticsPath.setEndCheckPointObject(endPointObject);
-            }
-            else {
+            if (path.getEndCheckPoint() != null) {
+                try {
+                    LogisticsCheckPointObject endPointObject = dataAPIClient.get("logisticscheckpoint/{id}", LogisticsCheckPointObject.class, path.getEndCheckPoint());
+                    logisticsPath.setEndCheckPointObject(endPointObject);
+                }
+                catch (Exception ex) {
+                    logisticsPath.setEndCheckPointObject(null);
+                    LOGGER.error("getLogisticsPathsOrderByStartDate error", ex);
+                }
+            } else {
                 logisticsPath.setEndCheckPointObject(null);
             }
 
