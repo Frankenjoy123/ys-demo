44d3171a5702ec2d9054a527aae6a46b7bad6953
Lijian <loye.qiu@gmail.com>
2015-05-15 14:56:54 +0800

    menu

--- web-enterprise/app/js/pages/index.js
+++ web-enterprise/app/js/pages/index.js
@@ -1,7 +1,7 @@
 (function () {
     var app = angular.module('root', [
         'ngRoute',
-        //'ngAnimate',
+        'ngAnimate',
         'YUNSOO_CONFIG',
         'interceptor',
         //'head',
@@ -71,89 +71,107 @@
     }]);
 
 
-    app.controller('rootCtrl', ['$scope', '$timeout', '$http', 'YUNSOO_CONFIG', function ($scope, $timeout, $http, YUNSOO_CONFIG) {
-        console.log('[root controller start]');
-        //YUNSOO_CONFIG
-        $scope.YUNSOO_CONFIG = YUNSOO_CONFIG;
+    app.controller('rootCtrl', ['$scope', '$timeout', '$http', 'YUNSOO_CONFIG', 'productBaseManageService',
+        function ($scope, $timeout, $http, YUNSOO_CONFIG, productBaseManageService) {
+            console.log('[root controller start]');
+            //YUNSOO_CONFIG
+            $scope.YUNSOO_CONFIG = YUNSOO_CONFIG;
 
-        //context
-        $scope.context || ($scope.context = {
-            getAccessToken: function () {
-                var accessToken = $.cookie(YUNSOO_CONFIG.NAME_ACCESS_TOKEN);
-                return typeof accessToken === 'string' ? accessToken : null;
-            }
-        });
+            //context
+            $scope.context || ($scope.context = {
+                getAccessToken: function () {
+                    var accessToken = $.cookie(YUNSOO_CONFIG.NAME_ACCESS_TOKEN);
+                    return typeof accessToken === 'string' ? accessToken : null;
+                }
+            });
 
-        //check authentication
-        if (!$scope.context.getAccessToken()) {
-            //redirect back to login page
-            window.location.href = 'login.html';
-        }
-
-        //utils
-        $scope.utils || ($scope.utils = {
-            formatDateString: function (value) {
-                return new DateTime(value).toString('yyyy-MM-dd HH:mm:ss');
-            },
-            logout: function () {
-                console.log('[logout]');
-                $.removeCookie(YUNSOO_CONFIG.NAME_ACCESS_TOKEN, {path: '/'});
+            //check authentication
+            if (!$scope.context.getAccessToken()) {
+                //redirect back to login page
                 window.location.href = 'login.html';
-            },
-            lock: function () {
-                console.log('[lock screen]');
-                $.niftyNoty({
-                    type: 'warning',
-                    message: '锁屏界面开发中...',
-                    container: 'floating',
-                    timer: 3000
-                });
-            },
-            /**
-             * shortcut of $.niftyNoty, show floating notification on the top right
-             * @param type string ["info", "primary", "success", "warning", "danger", "mint", "purple", "pink", "dark"]
-             * @param message string
-             * @param title string
-             */
-            notification: function (type, message, title) {
-                $.niftyNoty && $.niftyNoty({
-                    type: type,
-                    container: 'floating',
-                    title: title,
-                    message: message,
-                    timer: 3000
-                });
-            },
-            /**
-             * shortcut of $.niftyNoty, show alert, default in page top
-             * @param type string ["info", "primary", "success", "warning", "danger", "mint", "purple", "pink", "dark"]
-             * @param message string
-             * @param container string ["floating", "page"] | "jQuery selector"
-             */
-            alert: function (type, message, container) {
-                $.niftyNoty && $.niftyNoty({
-                    type: type,
-                    container: container || 'page',
-                    message: message,
-                    timer: 3000
-                });
             }
-        });
 
-        //load current account info
-        $http.get('/api/account/current').success(function (data) {
-            $scope.context.account = data;
-            console.log('[get current account]', data);
-        }).error(function (data, code) {
-            console.log('[get current account]', 'failed', code);
-        });
+            $scope.productKeyCreditSum = {
+                total: 0,
+                remain: 0,
+                percentage: 0
+            };
 
-        //show welcome message
-        $timeout(function () {
-            $scope.utils.notification('info', '欢迎登陆云溯管理平台');
-        }, 3000);
+            //utils
+            $scope.utils || ($scope.utils = {
+                formatDateString: function (value) {
+                    return new DateTime(value).toString('yyyy-MM-dd HH:mm:ss');
+                },
+                logout: function () {
+                    console.log('[logout]');
+                    $.removeCookie(YUNSOO_CONFIG.NAME_ACCESS_TOKEN, {path: '/'});
+                    window.location.href = 'login.html';
+                },
+                lock: function () {
+                    console.log('[lock screen]');
+                    $.niftyNoty({
+                        type: 'warning',
+                        message: '锁屏界面开发中...',
+                        container: 'floating',
+                        timer: 3000
+                    });
+                },
+                /**
+                 * shortcut of $.niftyNoty, show floating notification on the top right
+                 * @param type string ["info", "primary", "success", "warning", "danger", "mint", "purple", "pink", "dark"]
+                 * @param message string
+                 * @param title string
+                 */
+                notification: function (type, message, title) {
+                    $.niftyNoty && $.niftyNoty({
+                        type: type,
+                        container: 'floating',
+                        title: title,
+                        message: message,
+                        timer: 3000
+                    });
+                },
+                /**
+                 * shortcut of $.niftyNoty, show alert, default in page top
+                 * @param type string ["info", "primary", "success", "warning", "danger", "mint", "purple", "pink", "dark"]
+                 * @param message string
+                 * @param container string ["floating", "page"] | "jQuery selector"
+                 */
+                alert: function (type, message, container) {
+                    $.niftyNoty && $.niftyNoty({
+                        type: type,
+                        container: container || 'page',
+                        message: message,
+                        timer: 3000
+                    });
+                }
+            });
 
-        console.log('[root controller end]');
-    }]);//end of controller
+            //load current account info
+            $http.get('/api/account/current').success(function (data) {
+                $scope.context.account = data;
+                console.log('[get current account]', data);
+            }).error(function (data, code) {
+                console.log('[get current account]', 'failed', code);
+            });
+
+            //load product key credit for product key widget on the main menu
+            productBaseManageService.getProductKeyCredits(function (data) {
+                var sum = $scope.productKeyCreditSum;
+                $.each(data, function (i, item) {
+                    sum.total += item.total;
+                    sum.remain += item.remain;
+                });
+                sum.percentage = (sum.remain * 100 / sum.total) | 0;
+                console.log('[get productKeyCreditSum]: ', sum);
+            });
+
+            //show welcome message
+            $timeout(function () {
+                $scope.utils.notification('info', '欢迎登陆云溯管理平台');
+            }, 3000);
+
+            console.log('[root controller end]');
+        }]);//end of controller
 
 })();
