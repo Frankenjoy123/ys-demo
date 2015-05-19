df7c50fd8a69e499b165fe565cc07d9a99869a17
Jerry <chenruchang2002@163.com>
2015-05-19 18:07:43 +0800

    add update password function

--- web-enterprise/app/js/pages/setting/setting.js
+++ web-enterprise/app/js/pages/setting/setting.js
@@ -3,13 +3,9 @@
 
     app.factory("settingService", ["$http", function ($http) {
         return {
-            getInfo: function (productKey, fnSuccess, fnError) {
-                $http.get("/api/" + productKey)
-                    .success(function (data) {
-                        fnSuccess(data);
-                    }).error(function (data, state) {
-                        fnSuccess();
-                    });
+            updatePassword: function (passwordObj, fnSuccess, fnFail) {
+                $http.post("/api/account/current/password", passwordObj).success(fnSuccess).error(fnFail);
+                return this;
             }
         };
     }]);
@@ -20,6 +16,8 @@
         $scope.currentPassword = "";
         $scope.confirmPassword = "";
 
+
+
         $scope.submit = function () {
             if ($scope.originalPassword == "") {
                 $scope.utils.alert('warning', '请输入原密码！');
@@ -29,14 +27,21 @@
                 $scope.utils.alert('warning', '请输入修改密码！');
                 return;
             }
-            if ($scope.currentPassword.length < 6) {
-                $scope.utils.alert('warning', '新密码不能少于六位！');
-                return;
-            }
             if ($scope.currentPassword != $scope.confirmPassword) {
                 $scope.utils.alert('warning', '确认密码与输入密码不一致！');
                 return;
             }
+
+            var passwordObj = {
+                old_password: $scope.originalPassword,
+                new_password: $scope.currentPassword
+            };
+
+            settingService.updatePassword(passwordObj, function (data) {
+                $scope.utils.alert('success', '修改密码成功！');
+            }, function (data) {
+                $scope.utils.alert('warning', '修改密码失败！');
+            });
         };
     }]);
 })();
