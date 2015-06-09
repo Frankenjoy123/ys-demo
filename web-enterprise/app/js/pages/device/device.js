a9fad88d40e2b7e1622a633ddab99d26f9e09afb
Jerry <chenruchang2002@163.com>
2015-06-09 12:36:34 +0800

    optimize the device manage page

--- web-enterprise/app/js/pages/device/device.js
+++ web-enterprise/app/js/pages/device/device.js
@@ -5,14 +5,26 @@
         return {
             getDevices: function (dataTable, orgId, fnSuccess) {
                 var url = '/api/device/org/' + orgId + '?';
-
                 url += dataTable.toString();
                 $http.get(url).success(fnSuccess);
+
+                return this;
+            },
+            getCurrentOrgAccounts: function (fnSuccess) {
+                var url = '/api/account';
+                $http.get(url).success(fnSuccess);
+
+                return this;
             }
         };
     }]);
 
     app.controller("deviceCtrl", ["$scope", "deviceService", "$timeout", function ($scope, deviceService, $timeout) {
+
+        $scope.curOrgAccounts = '';
+        $scope.deviceComment = '';
+        $scope.deviceName = '';
+        $scope.selectAccount = '';
 
         (function newDataTable() {
             if ($scope.context.account) {
@@ -36,18 +48,45 @@
             }
         })();
 
-        $scope.deviceAuth = function (){
+        deviceService.getCurrentOrgAccounts(function (data) {
+            $scope.curOrgAccounts = data;
+        });
+
+        $scope.deviceAuth = function () {
+
+            if ($scope.selectAccount == '') {
+                $('#divSelectAccount').addClass('has-error').addClass('has-feedback');
+                return;
+            }
+
+            if ($scope.deviceName == '') {
+                $('#divDeviceName').addClass('has-error').addClass('has-feedback');
+                return;
+            }
+
+            $('#divDeviceName').addClass('has-success').addClass('has-feedback');
+            $('#divSelectAccount').addClass('has-success').addClass('has-feedback');
 
             $("#authQRCode").html('');
+
+            var postObject = {};
+            postObject.selectAccount = $scope.selectAccount;
+            postObject.deviceName = $scope.deviceName;
+            postObject.deviceComment = $scope.deviceComment;
 
             $scope.qrcode = $("#authQRCode").qrcode({
                 render: "table", //table方式
                 width: 300, //宽度
-                height:300, //高度
+                height: 300, //高度
                 foreground: "#337ab7",//前景颜色
                 text: "token" //任意内容
             });
-        }
+        };
+
+        $scope.cancelDeviceAuth = function () {
+
+
+        };
 
     }]);
 })();
