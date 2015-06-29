550d1eabe157c24249d5467a8c7d41ec90d4f73c
Jerry <chenruchang2002@163.com>
2015-06-29 14:46:29 +0800

    optimize the report pages

--- web-enterprise/app/js/pages/echarts/echarts-bar.js
+++ web-enterprise/app/js/pages/echarts/echarts-bar.js
@@ -1,69 +1,139 @@
 (function () {
-    var app = angular.module('root');
+  var app = angular.module('root');
 
-    app.factory("echartsBarService", ["$http", function ($http) {
-        return {
-            getDevices: function (dataTable, orgId, fnSuccess) {
-                var url = '/api/device/org/' + orgId + '?';
-                url += dataTable.toString();
-                $http.get(url).success(fnSuccess);
+  app.factory("echartsBarService", ["$http", function ($http) {
+    return {
+      getQRCode: function (peirod, fnSuccess, fnError) {
+        var url = '/api/report/myorganization/product_qrcode_count/' + peirod;
+        $http.get(url).success(fnSuccess).error(fnError);
 
-                return this;
-            }
-        };
-    }]);
+        return this;
+      }
+    };
+  }]);
 
-    app.controller("echartsBarCtrl", ["$scope", "echartsBarService", "$timeout", function ($scope, echartsBarService, $timeout) {
+  app.controller("echartsBarCtrl", ["$scope", "echartsBarService", "$timeout", function ($scope, echartsBarService, $timeout) {
 
-        $timeout(function () {
+    $scope.monDays = [];
+    $scope.currDay = getCurrDay();
 
-            var echartBar = echarts.init($('#echartBar')[0]);
+    var getQRCode = function (data) {
 
-            var option = {
-                title: {
-                    text: '产品贴码统计'
-                },
-                tooltip: {
-                    trigger: 'axis'
-                },
-                legend: {
-                    data: ['产品贴码统计']
-                },
-                toolbox: {
-                    show: true,
-                    feature: {
-                        mark: {show: true},
-                        dataView: {show: true, readOnly: false},
-                        magicType: {show: true, type: ['line', 'bar']},
-                        restore: {show: true},
-                        saveAsImage: {show: true}
-                    }
-                },
-                calculable: true,
-                xAxis: [
-                    {
-                        type: 'category',
-                        show: true,
-                        data: ['小儿护脑退热贴', '成人广东护脑退热贴', '大闸蟹', '菲律宾凤梨', '生理性海水鼻腔喷雾器', '小儿腹泻理疗贴', '依云矿泉水', '远红外风湿骨痛贴']
-                    }
-                ],
-                yAxis: [
-                    {
-                        type: 'value'
-                    }
-                ],
-                series: [
-                    {
-                        name: '产品贴码统计',
-                        type: 'bar',
-                        data: [56, 6, 35, 2, 7, 24, 4, 9]
-                    }
-                ]
-            };
+      var echartBar = echarts.init($('#echartBar')[0]);
 
-            echartBar.setOption(option);
+      var option = {
+        title: {
+          text: '产品贴码统计'
+        },
+        tooltip: {
+          trigger: 'axis'
+        },
+        legend: {
+          data: ['产品贴码统计']
+        },
+        toolbox: {
+          show: true,
+          feature: {
+            mark: {show: true},
+            dataView: {show: true, readOnly: false},
+            magicType: {show: true, type: ['line', 'bar']},
+            restore: {show: true},
+            saveAsImage: {show: true}
+          }
+        },
+        calculable: true,
+        xAxis: [
+          {
+            type: 'category',
+            show: true,
+            data: data.dimensions.values
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ],
+        series: [
+          {
+            name: '产品贴码统计',
+            type: 'bar',
+            data: data.data
+          }
+        ]
+      };
 
-        }, 0);
+      echartBar.setOption(option);
+    };
 
-    }]);
+    $scope.getData = function (data) {
+      echartsBarService.getQRCode(getDateStr(data), getQRCode, function () {
+        $scope.utils.alert('info', getDateStr(data) + '该日数据不存在');
+
+        var data1 = {};
+        data1.data = [];
+        getQRCode(data1);
+      });
+    };
+
+    function getDateStr(day) {
+
+      var date = new Date();
+      var xYear = date.getYear();
+      xYear = xYear + 1900;
+
+      var xMonth = date.getMonth() + 1;
+      if (xMonth < 10) {
+        xMonth = "0" + xMonth;
+      }
+
+      var xDay = getCurrDay();
+      if (day != undefined) {
+        xDay = day;
+      }
+
+      return xYear + xMonth + xDay;
+    };
+
+    function getDays() {
+      var date = new Date();
+      var y = date.getFullYear();
+      var m = date.getMonth() + 1;
+      if (m == 2) {
+        return y % 4 == 0 ? 29 : 28;
+      } else if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) {
+        return 31;
+      } else {
+        return 30;
+      }
+    };
+
+    function getCurrDay() {
+      var date = new Date();
+      var d = date.getDate();
+      if (d < 10) {
+        d = "0" + d;
+      }
+      return d;
+    };
+
+    for (var i = 1; i <= getDays(); i++) {
+
+      var day = 0;
+
+      if (i < 10)
+        day = '0' + i;
+      else
+        day = i;
+
+      $scope.monDays.push(day);
+    }
+
+    echartsBarService.getQRCode(getDateStr(), getQRCode, function () {
+      $scope.utils.alert('info', getDateStr() + '该日数据不存在');
+      var data = {};
+      data.data = [];
+      getQRCode(data);
+    });
+  }]);
 })();
