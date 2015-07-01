2e421c48c42a2579594aacb65a0a27c46b50edd1
Jerry <chenruchang2002@163.com>
2015-07-01 14:19:46 +0800

    optimize the chart pages

--- web-enterprise/app/js/pages/echarts/echarts-bar.js
+++ web-enterprise/app/js/pages/echarts/echarts-bar.js
@@ -17,6 +17,20 @@
     var date = new Date();
     $scope.days = [];
     $scope.mons = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+    $scope.monsShow = {
+      '01': 'Jan.',
+      '02': 'Feb.',
+      '03': 'Mar.',
+      '04': 'Apr.',
+      '05': 'May.',
+      '06': 'Jun.',
+      '07': 'Jul.',
+      '08': 'Aug.',
+      '09': 'Sep.',
+      '10': 'Oct.',
+      '11': 'Nov.',
+      '12': 'Dec.'
+    };
     $scope.years = [];
 
     $scope.selYear = date.getCurrYear();
