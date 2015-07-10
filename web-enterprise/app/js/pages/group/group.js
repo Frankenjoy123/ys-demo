415d1148200eb071ea5ebb2a67dac316b46eca69
Jerry <chenruchang2002@163.com>
2015-07-10 11:06:07 +0800

    optimize the group page

--- web-enterprise/app/js/pages/group/group.js
+++ web-enterprise/app/js/pages/group/group.js
@@ -13,6 +13,24 @@
 
         return this;
       },
+      getCurrentOrgAccounts: function (fnSuccess) {
+        var url = '/api/account';
+        $http.get(url).success(fnSuccess);
+
+        return this;
+      },
+      getCurrentGroupAccounts: function (groupId, fnSuccess) {
+        var url = '/api/group/' + groupId + '/account';
+        $http.get(url).success(fnSuccess);
+
+        return this;
+      },
+      addAccountsToGroup: function (groupId, accounts, fnSuccess, fnError) {
+        var url = '/api/group/' + groupId + '/account';
+        $http.put(url, accounts).success(fnSuccess).error(fnError);
+
+        return this;
+      },
       deleteGroup: function (groupId, fnSuccess, fnError) {
         $http.delete('/api/group/' + groupId).success(fnSuccess);
 
@@ -29,18 +47,23 @@
     'dataFilterService',
     function ($scope, $timeout, $route, groupService, dataFilterService) {
 
-      var group = $scope.group = {
-        name: '',
-        description: ''
+      var groupDatatable = {
+        sortable: {
+          target: '#sort-bar',
+          sort: 'createdDateTime,desc'
+        },
+        pageable: {
+          page: 0,
+          size: 20
+        },
+        flush: function (callback) {
+          groupService.getGroups(function (data, status, headers) {
+            callback({data: data, headers: headers});
+          });
+        }
       };
 
-      $scope.spinnerShow = false;
-
-      $timeout(function () {
-        $('#demo-cs-multiselect').chosen({width: '100%'});
-      }, 0);
-
-      $scope.groupPermission = {
+      var groupPermission = $scope.groupPermission = {
         dashBoardRead: '',
         productKeyRead: '',
         productKeyMng: '',
@@ -62,65 +85,143 @@
         passwordRead: ''
       };
 
-      $scope.deleteGroup = function (id) {
-        groupService.deleteGroup(id, function (data, status) {
-          $scope.utils.alert('info', '账号组删除成功');
+      groupService.getCurrentOrgAccounts(function (data) {
+        group.curOrgAccounts = data;
+      });
 
-          setTimeout("location.reload();", 1500);
-        });
-      };
+      var group = $scope.group = {
+        name: '',
+        description: '',
+        spinnerShow: false,
+        curDeleteGroup: '',
+        curDeleteGroupName: '',
+        curSelectGroup: '',
+        groupTable: new $scope.utils.DataTable(groupDatatable),
+        curOrgAccounts: '',
+        selectAccounts: [],
+        showGroupModal: function () {
+          $('#myModal').modal({backdrop: 'static', keyboard: false}).on("hidden.bs.modal", function () {
+            $(this).removeData("bs.modal");
+          });
+        },
+        showAddAccountModal: function (id) {
+          group.curSelectGroup = id;
 
-      var groupDatatable = {
-        sortable: {
-          target: '#sort-bar',
-          sort: 'createdDateTime,desc'
+          groupService.getCurrentGroupAccounts(id, function (data) {
+
+            if (group.curOrgAccounts != '') {
+
+              $.each(group.curOrgAccounts, function (name, value) {
+                var option = "<option value=" + value.id;
+
+                if (data != undefined && data.length > 0) {
+                  $.each(data, function (name1, value1) {
+                    if (value1.id == value.id) {
+                      option += " selected = 'selected'";
+                    }
+                  });
+                }
+
+                option += ">" + value.last_name + value.first_name + "</option>";
+                $('#selectAccount').append(option);
+              });
+            }
+
+            $('#selectAccount').chosen({width: '100%'});
+            $("#selectAccount").change(function () {
+              group.selectAccounts = $(this).val();
+            });
+
+            $('#addAccountsModal').modal({backdrop: 'static', keyboard: false}).on("hidden.bs.modal", function () {
+              $(this).removeData("bs.modal");
+            });
+          }, function () {
+            if (group.curOrgAccounts != '') {
+
+              $.each(group.curOrgAccounts, function (name, value) {
+                var option = "<option value=" + value.id + ">" + value.last_name + value.first_name + "</option>";
+                $('#selectAccount').append(option);
+              });
+            }
+
+            $('#selectAccount').chosen({width: '100%'});
+            $("#selectAccount").change(function () {
+              group.selectAccounts = $(this).val();
+            });
+
+            $('#addAccountsModal').modal({backdrop: 'static', keyboard: false}).on("hidden.bs.modal", function () {
+              $(this).removeData("bs.modal");
+            });
+          });
         },
-        pageable: {
-          page: 0,
-          size: 20
+        hideAddAccountsModal: function () {
+          group.curSelectGroup = '';
+          group.spinnerShow = false;
+          $("#selectAccount").chosen("destroy");
+          $("#selectAccount").html("");
+          $('#addAccountsModal').modal('hide');
         },
-        flush: function (callback) {
-          groupService.getGroups(function (data, status, headers) {
-            callback({data: data, headers: headers});
+        addAccountsToGroup: function () {
+          group.spinnerShow = true;
+
+          if (group.curSelectGroup != '') {
+            groupService.addAccountsToGroup(group.curSelectGroup, group.selectAccounts || [], function (data) {
+              group.hideAddAccountsModal();
+              $scope.utils.alert('info', '账号更新成功');
+            }, function (data) {
+              group.spinnerShow = false;
+              $scope.utils.alert('danger', '账号更新失败', '#addAccountsModal .modal-dialog', false);
+            });
+          }
+        },
+        deleteGroup: function (id, name) {
+          group.curDeleteGroup = id;
+          group.curDeleteGroupName = name;
+
+          $('#deleteConfirmDialog').modal({backdrop: 'static', keyboard: false}).on("hidden.bs.modal", function () {
+            $(this).removeData("bs.modal");
+          });
+        },
+        deleteGroupConfirm: function () {
+          if (group.curDeleteGroup != '') {
+            groupService.deleteGroup(group.curDeleteGroup, function (data, status) {
+              $('#deleteConfirmDialog').modal('hide');
+              $scope.utils.alert('info', '账号组删除成功');
+              group.groupTable = new $scope.utils.DataTable(groupDatatable);
+            });
+          }
+        },
+        deleteGroupCancel: function () {
+          group.curDeleteGroup = '';
+          $('#deleteConfirmDialog').modal('hide');
+        },
+        createGroup: function (isValid) {
+          if (!isValid) {
+            $scope.utils.alert('info', '页面验证有错误，请返回检查');
+            return;
+          }
+
+          group.spinnerShow = true;
+
+          var groupObj = {
+            name: group.name,
+            description: group.description
+          };
+
+          groupService.createGroup(groupObj, function (data) {
+
+            group.spinnerShow = false;
+
+            $('#myModal').modal('hide');
+
+            $scope.utils.alert('info', '账号组创建成功');
+
+            this.groupTable = new $scope.utils.DataTable(groupDatatable);
+          }, function (error) {
+            $scope.utils.alert('danger', '账号组创建失败', '#myModal .modal-dialog', false);
           });
         }
       };
-
-      $scope.submit = function (isValid) {
-        if (!isValid) {
-          $scope.utils.alert('info', '页面验证有错误，请返回检查');
-          return;
-        }
-
-        $scope.spinnerShow = true;
-
-        groupService.createGroup(group, function (data) {
-
-          $scope.spinnerShow = false;
-
-          $('#myModal').modal('hide');
-
-          $scope.utils.alert('info', '账号组创建成功');
-
-          $scope.groupTable = new $scope.utils.DataTable(groupDatatable);
-        }, function (error) {
-          $.niftyNoty({
-            type: 'purple',
-            icon: 'fa fa-check',
-            message: "账号组创建失败",
-            container: 'floating',
-            timer: 3000
-          });
-        });
-      };
-
-      $scope.showGroupModal = function () {
-        $('#myModal').modal({backdrop: 'static', keyboard: false}).on("hidden.bs.modal", function () {
-          $(this).removeData("bs.modal");
-        });
-      };
-
-      $scope.groupTable = new $scope.utils.DataTable(groupDatatable);
 
     }]);
 })();
