4c112ad09c884cda08cf0f8b316b7ef32627b9ad
Lijian <loye.qiu@gmail.com>
2015-07-20 16:45:32 +0800

    product base versions

---
+++ common-data/src/main/java/com/yunsoo/common/data/object/ProductBaseVersionsObject.java
@@ -0,0 +1,210 @@
+package com.yunsoo.common.data.object;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.yunsoo.common.data.databind.DateTimeJsonDeserializer;
+import com.yunsoo.common.data.databind.DateTimeJsonSerializer;
+import org.joda.time.DateTime;
+
+import java.util.List;
+
+/**
+ * Created by  : Lijian
+ * Created on  : 2015/7/20
+ * Descriptions:
+ */
+public class ProductBaseVersionsObject {
+
+    @JsonProperty("id")
+    private String id;
+
+    @JsonProperty("product_base_id")
+    private String productBaseId;
+
+    @JsonProperty("version")
+    private String version;
+
+    @JsonProperty("status_code")
+    private String statusCode;
+
+    @JsonProperty("org_id")
+    private String orgId;
+
+    @JsonProperty("category_id")
+    private String categoryId;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("description")
+    private String description;
+
+    @JsonProperty("barcode")
+    private String barcode;
+
+    @JsonProperty("product_key_type_codes")
+    private List<String> productKeyTypeCodes;
+
+    @JsonProperty("shelf_life")
+    private Integer shelfLife;
+
+    @JsonProperty("shelf_life_interval")
+    private String shelfLifeInterval;
+
+    @JsonProperty("child_product_count")
+    private Integer childProductCount;
+
+    @JsonProperty("comments")
+    private String comments;
+
+    @JsonProperty("review_comments")
+    private String reviewComments;
+
+    @JsonSerialize(using = DateTimeJsonSerializer.class)
+    @JsonDeserialize(using = DateTimeJsonDeserializer.class)
+    @JsonProperty("created_datetime")
+    private DateTime createdDateTime;
+
+    @JsonSerialize(using = DateTimeJsonSerializer.class)
+    @JsonDeserialize(using = DateTimeJsonDeserializer.class)
+    @JsonProperty("modified_datetime")
+    private DateTime modifiedDateTime;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProductBaseId() {
+        return productBaseId;
+    }
+
+    public void setProductBaseId(String productBaseId) {
+        this.productBaseId = productBaseId;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(String statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getOrgId() {
+        return orgId;
+    }
+
+    public void setOrgId(String orgId) {
+        this.orgId = orgId;
+    }
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public List<String> getProductKeyTypeCodes() {
+        return productKeyTypeCodes;
+    }
+
+    public void setProductKeyTypeCodes(List<String> productKeyTypeCodes) {
+        this.productKeyTypeCodes = productKeyTypeCodes;
+    }
+
+    public Integer getShelfLife() {
+        return shelfLife;
+    }
+
+    public void setShelfLife(Integer shelfLife) {
+        this.shelfLife = shelfLife;
+    }
+
+    public String getShelfLifeInterval() {
+        return shelfLifeInterval;
+    }
+
+    public void setShelfLifeInterval(String shelfLifeInterval) {
+        this.shelfLifeInterval = shelfLifeInterval;
+    }
+
+    public Integer getChildProductCount() {
+        return childProductCount;
+    }
+
+    public void setChildProductCount(Integer childProductCount) {
+        this.childProductCount = childProductCount;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public String getReviewComments() {
+        return reviewComments;
+    }
+
+    public void setReviewComments(String reviewComments) {
+        this.reviewComments = reviewComments;
+    }
+
+    public DateTime getCreatedDateTime() {
+        return createdDateTime;
+    }
+
+    public void setCreatedDateTime(DateTime createdDateTime) {
+        this.createdDateTime = createdDateTime;
+    }
+
+    public DateTime getModifiedDateTime() {
+        return modifiedDateTime;
+    }
+
+    public void setModifiedDateTime(DateTime modifiedDateTime) {
+        this.modifiedDateTime = modifiedDateTime;
+    }
+}
