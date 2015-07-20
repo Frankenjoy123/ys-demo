2cb825349439c20544c23c157defd8fb82de02e5
Lijian <loye.qiu@gmail.com>
2015-07-20 16:00:03 +0800

    ProductBase & ProductBaseVersions

---
+++ data-service/src/main/java/com/yunsoo/data/service/entity/ProductBaseEntity.java
@@ -0,0 +1,199 @@
+package com.yunsoo.data.service.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+import org.joda.time.DateTime;
+
+import javax.persistence.*;
+
+/**
+ * Created by  : Lijian
+ * Created on  : 2015/7/9
+ * Descriptions:
+ */
+@Entity
+@Table(name = "product_base")
+public class ProductBaseEntity {
+
+    @Id
+    @GeneratedValue(generator = "idGenerator")
+    @GenericGenerator(name = "idGenerator", strategy = "com.yunsoo.data.service.util.IdGenerator")
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "version")
+    private String version;
+
+    @Column(name = "status_code")
+    private String statusCode;
+
+    @Column(name = "org_id")
+    private String orgId;
+
+    @Column(name = "category_id")
+    private Integer categoryId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "barcode")
+    private String barcode;
+
+    @Column(name = "product_key_type_codes")
+    private String productKeyTypeCodes;
+
+    @Column(name = "shelf_life")
+    private Integer shelfLife;
+
+    @Column(name = "shelf_life_interval")
+    private String shelfLifeInterval;
+
+    @Column(name = "child_product_count")
+    private Integer childProductCount;
+
+    @Column(name = "comments")
+    private String comments;
+
+    @Column(name = "is_deleted")
+    private Boolean isDeleted;
+
+    @Column(name = "created_datetime")
+    @Type(type = "org.jadira.usertype.dateandtime.joda.PersistentDateTime")
+    private DateTime createdDateTime;
+
+    @Column(name = "modified_datetime")
+    @Type(type = "org.jadira.usertype.dateandtime.joda.PersistentDateTime")
+    private DateTime modifiedDateTime;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(String statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getOrgId() {
+        return orgId;
+    }
+
+    public void setOrgId(String orgId) {
+        this.orgId = orgId;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public String getProductKeyTypeCodes() {
+        return productKeyTypeCodes;
+    }
+
+    public void setProductKeyTypeCodes(String productKeyTypeCodes) {
+        this.productKeyTypeCodes = productKeyTypeCodes;
+    }
+
+    public Integer getShelfLife() {
+        return shelfLife;
+    }
+
+    public void setShelfLife(Integer shelfLife) {
+        this.shelfLife = shelfLife;
+    }
+
+    public String getShelfLifeInterval() {
+        return shelfLifeInterval;
+    }
+
+    public void setShelfLifeInterval(String shelfLifeInterval) {
+        this.shelfLifeInterval = shelfLifeInterval;
+    }
+
+    public Integer getChildProductCount() {
+        return childProductCount;
+    }
+
+    public void setChildProductCount(Integer childProductCount) {
+        this.childProductCount = childProductCount;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public Boolean getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(Boolean isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
+    public DateTime getCreatedDateTime() {
+        return createdDateTime;
+    }
+
+    public void setCreatedDateTime(DateTime createdDateTime) {
+        this.createdDateTime = createdDateTime;
+    }
+
+    public DateTime getModifiedDateTime() {
+        return modifiedDateTime;
+    }
+
+    public void setModifiedDateTime(DateTime modifiedDateTime) {
+        this.modifiedDateTime = modifiedDateTime;
+    }
+}
--- data-service/src/main/resources/sql/tables/product_base_tables.sql
+++ data-service/src/main/resources/sql/tables/product_base_tables.sql
@@ -1,21 +1,21 @@
 #product_base
 CREATE TABLE product_base
 (
-  id                     VARCHAR(20) PRIMARY KEY  NOT NULL,
-  version                INT                      NOT NULL,
-  status_code            VARCHAR(20)              NOT NULL,
-  org_id                 VARCHAR(20)              NOT NULL,
-  category_id            INT                      NOT NULL,
-  name                   VARCHAR(255)             NOT NULL,
+  id                     VARCHAR(20) PRIMARY KEY        NOT NULL,
+  version                INT DEFAULT 1                  NOT NULL,
+  status_code            VARCHAR(20)                    NOT NULL,
+  org_id                 VARCHAR(20)                    NOT NULL,
+  category_id            INT                            NOT NULL,
+  name                   VARCHAR(255)                   NOT NULL,
   description            VARCHAR(255),
-  shelf_life             INT                      NOT NULL,
-  shelf_life_interval    VARCHAR(8)               NOT NULL,
-  product_key_type_codes VARCHAR(45)              NOT NULL,
-  barcode                VARCHAR(100)             NOT NULL,
+  barcode                VARCHAR(30)                    NOT NULL,
+  product_key_type_codes VARCHAR(100)                   NOT NULL,
+  shelf_life             INT,
+  shelf_life_interval    VARCHAR(8),
   child_product_count    INT,
   comments               VARCHAR(500),
-  is_deleted             BIT DEFAULT b'0'         NOT NULL,
-  created_datetime       DATETIME                 NOT NULL,
+  is_deleted             BIT DEFAULT b'0'               NOT NULL,
+  created_datetime       DATETIME                       NOT NULL,
   modified_datetime      DATETIME
 );
 
@@ -23,22 +23,22 @@ CREATE TABLE product_base
 #product_base_versions
 CREATE TABLE product_base_versions
 (
-  id                     VARCHAR(20) PRIMARY KEY NOT NULL,
-  product_base_id        VARCHAR(20)             NOT NULL,
-  version                INT                     NOT NULL,
-  status_code     VARCHAR(20) NOT NULL,
-  org_id                 VARCHAR(20)             NOT NULL,
-  category_id            INT                     NOT NULL,
-  name                   VARCHAR(255)            NOT NULL,
-  description     VARCHAR(255),
-  shelf_life             INT                     NOT NULL,
-  shelf_life_interval    VARCHAR(8)              NOT NULL,
-  product_key_type_codes VARCHAR(45)             NOT NULL,
-  barcode                VARCHAR(100)            NOT NULL,
+  id                     VARCHAR(20) PRIMARY KEY        NOT NULL,
+  product_base_id        VARCHAR(20)                    NOT NULL,
+  version                INT                            NOT NULL,
+  status_code            VARCHAR(20)                    NOT NULL,
+  org_id                 VARCHAR(20)                    NOT NULL,
+  category_id            INT                            NOT NULL,
+  name                   VARCHAR(255)                   NOT NULL,
+  description            VARCHAR(255),
+  barcode                VARCHAR(30)                    NOT NULL,
+  product_key_type_codes VARCHAR(100)                   NOT NULL,
+  shelf_life             INT,
+  shelf_life_interval    VARCHAR(8),
   child_product_count    INT,
-  comments        VARCHAR(500),
-  review_comments VARCHAR(4000),
-  created_datetime       DATETIME                NOT NULL,
+  comments               VARCHAR(500),
+  review_comments        VARCHAR(4000),
+  created_datetime       DATETIME                       NOT NULL,
   modified_datetime      DATETIME
 );
 
