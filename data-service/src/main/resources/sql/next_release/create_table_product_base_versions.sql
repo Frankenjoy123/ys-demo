0a2cca8d9b444339f4ce3f79e5aefd6373bad2b8
Lijian <loye.qiu@gmail.com>
2015-07-22 17:29:32 +0800

    split sql script

---
+++ data-service/src/main/resources/sql/next_release/create_table_product_base_versions.sql
@@ -0,0 +1,24 @@
+#product_base_versions
+CREATE TABLE product_base_versions
+(
+  id                     VARCHAR(20) PRIMARY KEY                           NOT NULL,
+  product_base_id        VARCHAR(20)                                       NOT NULL,
+  version                INT                                               NOT NULL,
+  status_code            VARCHAR(20)                                       NOT NULL,
+  org_id                 VARCHAR(20)                                       NOT NULL,
+  category_id            VARCHAR(20)                                       NOT NULL,
+  name                   VARCHAR(255)                                      NOT NULL,
+  description            VARCHAR(255),
+  barcode                VARCHAR(30)                                       NOT NULL,
+  product_key_type_codes VARCHAR(100)                                      NOT NULL,
+  shelf_life             INT,
+  shelf_life_interval    VARCHAR(8),
+  child_product_count    INT,
+  comments               VARCHAR(500),
+  review_comments        VARCHAR(4000),
+  created_account_id     VARCHAR(20)                                       NOT NULL,
+  created_datetime       DATETIME DEFAULT CURRENT_TIMESTAMP                NOT NULL,
+  modified_account_id    VARCHAR(20),
+  modified_datetime      DATETIME
+);
+CREATE UNIQUE INDEX product_base_id_version ON product_base_versions (product_base_id, version);
