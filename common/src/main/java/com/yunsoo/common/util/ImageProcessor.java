a29df996159d993904d2ad83acb243f293eb24cc
Lijian <loye.qiu@gmail.com>
2015-07-24 15:46:21 +0800

    ImageProcessor

---
+++ common/src/main/java/com/yunsoo/common/util/ImageProcessor.java
@@ -0,0 +1,77 @@
+package com.yunsoo.common.util;
+
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageInputStream;
+import javax.imageio.stream.ImageOutputStream;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Created by  : Lijian
+ * Created on  : 2015/7/24
+ * Descriptions:
+ */
+public class ImageProcessor {
+
+    private BufferedImage bufferedImage;
+
+    /**
+     * @param inputStream InputStream
+     * @return this
+     * @throws IOException
+     */
+    public ImageProcessor read(InputStream inputStream) throws IOException {
+        this.bufferedImage = ImageIO.read(inputStream);
+        return this;
+    }
+
+    /**
+     * @param inputStream     InputStream
+     * @param inputFormatName String [png|jpg|bmp|gif]
+     * @return this
+     * @throws IOException
+     */
+    public ImageProcessor read(InputStream inputStream, String inputFormatName) throws IOException {
+        ImageInputStream imageInputStream = ImageIO.createImageInputStream(inputStream);
+        ImageReader imageReader = ImageIO.getImageReadersByFormatName(inputFormatName).next();
+        imageReader.setInput(imageInputStream, true);
+        this.bufferedImage = imageReader.read(0);
+        return this;
+    }
+
+    /**
+     * @param outputStream     OutputStream
+     * @param outputFormatName String [png|jpg|bmp|gif]
+     * @return this
+     * @throws IOException
+     */
+    public ImageProcessor write(OutputStream outputStream, String outputFormatName) throws IOException {
+        if (this.bufferedImage != null) {
+            ImageOutputStream imageOutputStream = ImageIO.createImageOutputStream(outputStream);
+            ImageWriter imageWriter = ImageIO.getImageWritersByFormatName(outputFormatName).next();
+            imageWriter.setOutput(imageOutputStream);
+            imageWriter.write(bufferedImage);
+            imageWriter.dispose();
+            imageOutputStream.close();
+        }
+        return this;
+    }
+
+    public ImageProcessor crop(int x, int y, int width, int height) {
+        this.bufferedImage = this.bufferedImage.getSubimage(x, y, width, height);
+        return this;
+    }
+
+    public ImageProcessor resize(int width, int height) {
+        Image image = this.bufferedImage.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        BufferedImage newBufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        newBufferedImage.getGraphics().drawImage(image, 0, 0, null);
+        this.bufferedImage = newBufferedImage;
+        return this;
+    }
+}
---
+++ common/src/test/java/com/yunsoo/common/util/ImageProcessorTest.java
@@ -0,0 +1,42 @@
+package com.yunsoo.common.util;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * Created by  : Lijian
+ * Created on  : 2015/7/23
+ * Descriptions:
+ */
+public class ImageProcessorTest {
+
+    @Test
+    public void test_all() throws IOException {
+//        InputStream inputStream = null;
+//        OutputStream outputStream = null;
+//        try {
+//            String fileName = "D:\\g.txt";
+//            byte[] bytes = Files.readAllBytes(Paths.get(fileName));
+//            inputStream = new ByteArrayInputStream(Base64.decodeBase64(bytes));
+//            outputStream = new FileOutputStream("D:\\temp.png");
+//
+//            new ImageProcessor().read(inputStream)
+//                    .crop(240, 130, 150, 150)
+//                    .resize(100, 100)
+//                    .write(outputStream, "png");
+//
+//            inputStream.close();
+//            outputStream.close();
+//
+//        } catch (IOException e) {
+//            if (inputStream != null) {
+//                inputStream.close();
+//            }
+//            if (outputStream != null) {
+//                outputStream.close();
+//            }
+//            e.printStackTrace();
+//        }
+    }
+}
