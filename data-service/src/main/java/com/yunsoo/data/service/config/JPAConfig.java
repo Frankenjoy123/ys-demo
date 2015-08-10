1ab9c39c2c8d65e9e287cfc2ab070a8b6c6b614c
Lijian <loye.qiu@gmail.com>
2015-08-10 18:01:50 +0800

    data-service fix datasource

--- data-service/src/main/java/com/yunsoo/data/service/config/JPAConfig.java
+++ data-service/src/main/java/com/yunsoo/data/service/config/JPAConfig.java
@@ -7,13 +7,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 /**
@@ -24,6 +23,7 @@ import javax.sql.DataSource;
 @Configuration
 @Import(DALConfig.class)
 @EnableJpaRepositories(basePackages = "com.yunsoo.data.service.repository")
+@EnableTransactionManagement
 public class JPAConfig {
 
     @Autowired
@@ -31,33 +31,34 @@ public class JPAConfig {
     DataSource dataSource;
 
     @Bean
-    public EntityManagerFactory entityManagerFactory() {
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
+        hibernateJpaVendorAdapter.setShowSql(false);
+        hibernateJpaVendorAdapter.setGenerateDdl(false);
+        hibernateJpaVendorAdapter.setDatabase(Database.MYSQL);
 
-//        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-//        vendorAdapter.setDatabase(Database.MYSQL);
-//        vendorAdapter.setGenerateDdl(true);
         LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
-        factory.setJpaVendorAdapter(jpaVendorAdapter());
-        factory.setPackagesToScan("com.yunsoo.data.service.entity");
+        factory.setJpaVendorAdapter(hibernateJpaVendorAdapter);
         factory.setDataSource(dataSource);
+        factory.setPackagesToScan("com.yunsoo.data.service.entity");
         factory.setPersistenceUnitName("master");
         factory.afterPropertiesSet();
-        return factory.getObject();
+        return factory;
     }
 
     @Bean
     public PlatformTransactionManager transactionManager() {
         JpaTransactionManager txManager = new JpaTransactionManager();
-        txManager.setEntityManagerFactory(entityManagerFactory());
+        txManager.setEntityManagerFactory(entityManagerFactory().getObject());
         return txManager;
     }
-
-    @Bean
-    public JpaVendorAdapter jpaVendorAdapter() {
-        HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
-        hibernateJpaVendorAdapter.setShowSql(false);
-        hibernateJpaVendorAdapter.setGenerateDdl(false);
-        hibernateJpaVendorAdapter.setDatabase(Database.MYSQL);
-        return hibernateJpaVendorAdapter;
-    }
+//
+//    @Bean
+//    public JpaVendorAdapter jpaVendorAdapter() {
+//        HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
+//        hibernateJpaVendorAdapter.setShowSql(false);
+//        hibernateJpaVendorAdapter.setGenerateDdl(false);
+//        hibernateJpaVendorAdapter.setDatabase(Database.MYSQL);
+//        return hibernateJpaVendorAdapter;
+//    }
 }
