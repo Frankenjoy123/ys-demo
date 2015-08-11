a701bd4b0fda05717fe897910a11041a8ca532bd
seedwht <seedwht@gmail.com>
2015-08-11 17:36:35 +0800

    Add MessageEntity

---
+++ data-service/src/main/java/com/yunsoo/data/service/entity/MessageEntity.java
@@ -0,0 +1,121 @@
+package com.yunsoo.data.service.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+import org.joda.time.DateTime;
+
+import javax.persistence.*;
+
+/**
+ * Created by  : Haitao
+ * Created on  : 2015/8/11
+ * Descriptions:
+ */
+@Entity
+@Table(name = "message")
+public class MessageEntity {
+
+    @Id
+    @GeneratedValue(generator = "idGenerator")
+    @GenericGenerator(name = "idGenerator", strategy = "com.yunsoo.data.service.util.IdGenerator")
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "org_id")
+    private String orgId;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "status_code")
+    private String statusCode;
+
+    @Column(name = "type_code")
+    private String typeCode;
+
+    @Column(name = "created_account_id")
+    private String createdAccountId;
+
+    @Column(name = "created_datetime")
+    @Type(type = "org.jadira.usertype.dateandtime.joda.PersistentDateTime")
+    private DateTime createdDateTime;
+
+    @Column(name = "modified_account_id")
+    private String modifiedAccountId;
+
+    @Column(name = "modified_datetime")
+    @Type(type = "org.jadira.usertype.dateandtime.joda.PersistentDateTime")
+    private DateTime modifiedDateTime;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOrgId() {
+        return orgId;
+    }
+
+    public void setOrgId(String orgId) {
+        this.orgId = orgId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(String statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getTypeCode() {
+        return typeCode;
+    }
+
+    public void setTypeCode(String typeCode) {
+        this.typeCode = typeCode;
+    }
+
+    public String getCreatedAccountId() {
+        return createdAccountId;
+    }
+
+    public void setCreatedAccountId(String createdAccountId) {
+        this.createdAccountId = createdAccountId;
+    }
+
+    public DateTime getCreatedDateTime() {
+        return createdDateTime;
+    }
+
+    public void setCreatedDateTime(DateTime createdDateTime) {
+        this.createdDateTime = createdDateTime;
+    }
+
+    public String getModifiedAccountId() {
+        return modifiedAccountId;
+    }
+
+    public void setModifiedAccountId(String modifiedAccountId) {
+        this.modifiedAccountId = modifiedAccountId;
+    }
+
+    public DateTime getModifiedDateTime() {
+        return modifiedDateTime;
+    }
+
+    public void setModifiedDateTime(DateTime modifiedDateTime) {
+        this.modifiedDateTime = modifiedDateTime;
+    }
+}
