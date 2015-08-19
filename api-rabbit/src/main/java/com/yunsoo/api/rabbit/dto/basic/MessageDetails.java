f10b079b7168f5899f4dba8beeb9bb775c7a83c6
seedwht <seedwht@gmail.com>
2015-08-19 18:09:59 +0800

    [api-rabbit]: refactor message

---
+++ api-rabbit/src/main/java/com/yunsoo/api/rabbit/dto/basic/MessageDetails.java
@@ -0,0 +1,59 @@
+package com.yunsoo.api.rabbit.dto.basic;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Created by:  haitao
+ * Created on:  2015/8/12
+ * Descriptions:
+ */
+public class MessageDetails {
+
+    @JsonProperty("title")
+    private String title;
+
+    @JsonProperty("author")
+    private String author;
+
+    @JsonProperty("cover")
+    private String cover;
+
+    @JsonProperty("body")
+    private String body;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public MessageDetails() {
+    }
+
+}
