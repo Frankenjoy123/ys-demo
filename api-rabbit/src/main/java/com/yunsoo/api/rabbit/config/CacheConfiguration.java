29a70924b7de9b77abc9915ffaba4c88b3386339
yan <yan@yunsu.com>
2015-09-17 16:44:26 +0800

    1. fix issues of cache in api-rabbit

--- api-rabbit/src/main/java/com/yunsoo/api/rabbit/config/CacheConfiguration.java
+++ api-rabbit/src/main/java/com/yunsoo/api/rabbit/config/CacheConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.cache.interceptor.KeyGenerator;
-import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.cloud.aws.cache.config.annotation.CacheClusterConfig;
 import org.springframework.cloud.aws.cache.config.annotation.EnableElastiCache;
 import org.springframework.cloud.aws.context.annotation.ConditionalOnAwsCloudEnvironment;
