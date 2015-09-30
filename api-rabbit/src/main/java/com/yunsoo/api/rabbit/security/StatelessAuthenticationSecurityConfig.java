7cb07ed6a889abe3468241b4cf2d2719d71c4098
Lijian <loye.qiu@gmail.com>
2015-09-30 11:48:24 +0800

    [api-rabbit]: permission

--- api-rabbit/src/main/java/com/yunsoo/api/rabbit/security/StatelessAuthenticationSecurityConfig.java
+++ api-rabbit/src/main/java/com/yunsoo/api/rabbit/security/StatelessAuthenticationSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -45,9 +46,11 @@ public class StatelessAuthenticationSecurityConfig extends WebSecurityConfigurer
                 //.antMatchers("/favicon.ico").permitAll()
                 .antMatchers("/application/**").permitAll()
                 .antMatchers("/auth/**").permitAll()
+                .antMatchers("/message/**").permitAll()
                 .antMatchers("/organization/**").permitAll()
                 .antMatchers("/productbase/**").permitAll()
                 .antMatchers("/scan/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/user/*/gravatar").permitAll()
 
                 .antMatchers("/v2/api-docs").permitAll()
                 .antMatchers("/configuration/ui").permitAll()
