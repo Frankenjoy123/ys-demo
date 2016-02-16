e44614f2816ff2528b315c0c56f5e372629b3212
Lijian <loye.qiu@gmail.com>
2016-02-16 18:44:17 +0800

    data-api

--- data-service/src/main/resources/sql/tables/lookup_code.sql
+++ data-service/src/main/resources/sql/tables/lookup_code.sql
@@ -1,19 +1,3 @@
-INSERT INTO organization
-(id, name, type_code, status_code, description, details, created_account_id, created_datetime)
-VALUES
-  ('2k0r1l55i2rs5544wz5', '云溯科技', 'tech', 'available', '关注互联网+的力量!', '详细描述信息', '2k0rahgcybh0l5uxtep',
-   '2015-05-01 00:00:00');
-
-INSERT INTO account (id, org_id, identifier, status_code, first_name, last_name, email, phone, password, hash_salt, created_account_id, created_datetime, modified_account_id, modified_datetime)
-VALUES
-  ('0010000000000000000', '2k0r1l55i2rs5544wz5', 'system', 'available', '系统帐号', '云溯', 'it@yunsu.co', '', '', '',
-   '0010000000000000000', '2015-05-01 00:00:00', NULL, NULL);
-
-INSERT INTO user (id, device_id, phone, name, status_code, point, address, created_datetime)
-VALUES
-  ('0020000000000000000', '00000000000000000000000000000000', NULL, '匿名', 'enabled', 0, NULL, '2015-05-01 00:00:00');
-
---lookup
 INSERT INTO lookup_code (type_code, code, name, description, active)
 VALUES
   ('account_status', 'activated', '正常', '正常', TRUE),
@@ -106,4 +90,3 @@ VALUES
   ('user_point_transaction_status', 'rollback', '回滚', '事务出错，回滚', TRUE),
 
   ('user_point_transaction_type', 'sign_in_rewards', '签到', '签到积分回馈', TRUE);
-
