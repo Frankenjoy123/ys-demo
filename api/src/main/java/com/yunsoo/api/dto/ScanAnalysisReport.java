4b52163577c2b7e7a5f2735e3a4f53909c7c38e4
pagepinner <yqy0902@gmail.com>
2016-02-17 18:44:17 +0800

    提供报表的数据接口：包括扫码分析，地域分析

---
+++ api/src/main/java/com/yunsoo/api/dto/ScanAnalysisReport.java
@@ -0,0 +1,76 @@
+package com.yunsoo.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Dake Wang on 2016/2/4.
+ *
+ *
+ * {
+ *
+ *     date:[...],
+ *     data:{pv:[...], uv[...]}
+ *
+ * }
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ScanAnalysisReport {
+
+    @JsonProperty("date")
+    private String[] date;
+
+    @JsonProperty("data")
+    private PVUV data;
+
+
+    public String[] getDate() {
+        return date;
+    }
+
+    public void setDate(String[] date) {
+        this.date = date;
+    }
+
+    public PVUV getData() {
+        return data;
+    }
+
+    public void setData(PVUV data) {
+        this.data = data;
+    }
+
+    public final static class PVUV
+    {
+        @JsonProperty("pv")
+        private int[] pv;
+
+        @JsonProperty("uv")
+        private int[] uv;
+
+        public int[] getPv() {
+            return pv;
+        }
+
+        public void setPv(int[] pv) {
+            this.pv = pv;
+        }
+
+        public int[] getUv() {
+            return uv;
+        }
+
+        public void setUv(int[] uv) {
+            this.uv = uv;
+        }
+    }
+
+
+}
+
+
+
+
+
---
+++ common-data/src/main/java/com/yunsoo/common/data/object/ScanRecordAnalysisObject.java
@@ -0,0 +1,76 @@
+package com.yunsoo.common.data.object;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.yunsoo.common.data.databind.DateTimeJsonDeserializer;
+import com.yunsoo.common.data.databind.DateTimeJsonSerializer;
+import org.joda.time.DateTime;
+
+import java.io.Serializable;
+
+/**
+ * Created by:   Lijian
+ * Created on:   2015/4/17
+ * Descriptions:
+ */
+public class ScanRecordAnalysisObject implements Serializable {
+
+
+    @JsonSerialize(using = DateTimeJsonSerializer.class)
+    @JsonDeserialize(using = DateTimeJsonDeserializer.class)
+    @JsonProperty("scan_date")
+    private DateTime scanDate;
+
+    @JsonProperty("product_base_id")
+    private String productBaseId;
+
+    @JsonProperty("batch_id")
+    private String batchId;
+
+    @JsonProperty("pv")
+    private int pv;
+
+    @JsonProperty("uv")
+    private int uv;
+
+    public DateTime getScanDate() {
+        return scanDate;
+    }
+
+    public void setScanDate(DateTime scanDate) {
+        this.scanDate = scanDate;
+    }
+
+    public String getProductBaseId() {
+        return productBaseId;
+    }
+
+    public void setProductBaseId(String productBaseId) {
+        this.productBaseId = productBaseId;
+    }
+
+    public String getBatchId() {
+        return batchId;
+    }
+
+    public void setBatchId(String batchId) {
+        this.batchId = batchId;
+    }
+
+    public int getPv() {
+        return pv;
+    }
+
+    public void setPv(int pv) {
+        this.pv = pv;
+    }
+
+    public int getUv() {
+        return uv;
+    }
+
+    public void setUv(int uv) {
+        this.uv = uv;
+    }
+}
