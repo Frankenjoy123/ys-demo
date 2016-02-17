951474a30b31eccf81e88e8eed4ffd1993c45758
seedwht <seedwht@gmail.com>
2016-02-17 18:12:21 +0800

    [api] query marketing plan

--- api/src/main/java/com/yunsoo/api/controller/MarketingController.java
+++ api/src/main/java/com/yunsoo/api/controller/MarketingController.java
@@ -2,16 +2,22 @@ package com.yunsoo.api.controller;
 
 import com.yunsoo.api.domain.MarketingDomain;
 import com.yunsoo.api.domain.ProductDomain;
+import com.yunsoo.api.dto.Marketing;
 import com.yunsoo.api.dto.MktDrawRule;
 import com.yunsoo.api.security.TokenAuthenticationService;
 import com.yunsoo.common.data.object.MarketingObject;
 import com.yunsoo.common.data.object.MktDrawRuleObject;
+import com.yunsoo.common.web.client.Page;
 import com.yunsoo.common.web.exception.BadRequestException;
 import com.yunsoo.common.web.exception.NotFoundException;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 /**
  * Created by  : haitao
@@ -50,4 +56,31 @@ public class MarketingController {
         MktDrawRuleObject newMktDrawRuleObject = marketingDomain.createMktDrawRule(mktDrawRuleObject);
         return new MktDrawRule(newMktDrawRuleObject);
     }
+
+    //query marketing plan by org id
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public List<Marketing> getByFilter(@RequestParam(value = "org_id", required = false) String orgId,
+                                       Pageable pageable,
+                                       HttpServletResponse response) {
+        orgId = fixOrgId(orgId);
+        Page<MarketingObject> marketingPage = marketingDomain.getMarketingByOrgId(orgId, pageable);
+        if (pageable != null) {
+            response.setHeader("Content-Range", marketingPage.toContentRange());
+        }
+        List<Marketing> marketings = marketingPage.map(p -> {
+            Marketing marketing = new Marketing(p);
+            return marketing;
+        }).getContent();
+
+        return marketings;
+    }
+
+    private String fixOrgId(String orgId) {
+        if (orgId == null || "current".equals(orgId)) {
+            //current orgId
+            return tokenAuthenticationService.getAuthentication().getDetails().getOrgId();
+        }
+        return orgId;
+    }
+
 }
