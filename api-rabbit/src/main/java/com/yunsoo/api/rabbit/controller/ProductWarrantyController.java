433ca227c19ec9226dffd05c398ac77d950af706
Jerry <chenruchang2002@163.com>
2016-02-19 18:06:16 +0800

    add warranty form post

---
+++ api-rabbit/src/main/java/com/yunsoo/api/rabbit/controller/ProductWarrantyController.java
@@ -0,0 +1,36 @@
+package com.yunsoo.api.rabbit.controller;
+
+import com.yunsoo.api.rabbit.domain.ProductWarrantyDomain;
+import com.yunsoo.api.rabbit.dto.ProductWarranty;
+import com.yunsoo.api.rabbit.security.TokenAuthenticationService;
+import com.yunsoo.common.data.object.ProductWarrantyObject;
+import com.yunsoo.common.web.exception.BadRequestException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/productwarranty")
+public class ProductWarrantyController {
+
+    @Autowired
+    private TokenAuthenticationService tokenAuthenticationService;
+
+    @Autowired
+    private ProductWarrantyDomain productWarrantyDomain;
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductWarranty createProductWarranty(@RequestBody ProductWarranty productWarranty) {
+        if (productWarranty == null) {
+            throw new BadRequestException("productWarranty can not be null");
+        }
+        ProductWarrantyObject productWarrantyObject = productWarranty.toProductWarrantyObject();
+        if (productWarrantyObject == null) {
+            throw new BadRequestException("productWarrantyObject can not be null");
+        }
+
+        ProductWarrantyObject newObject = productWarrantyDomain.createProductWarranty(productWarrantyObject);
+        return new ProductWarranty(newObject);
+    }
+}
---
+++ api-rabbit/src/main/java/com/yunsoo/api/rabbit/domain/ProductWarrantyDomain.java
@@ -0,0 +1,21 @@
+package com.yunsoo.api.rabbit.domain;
+
+import com.yunsoo.common.data.object.ProductWarrantyObject;
+import com.yunsoo.common.web.client.RestClient;
+import org.joda.time.DateTime;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductWarrantyDomain {
+
+    @Autowired
+    private RestClient dataAPIClient;
+
+    public ProductWarrantyObject createProductWarranty(ProductWarrantyObject productWarrantyObject) {
+        productWarrantyObject.setId(null);
+        productWarrantyObject.setStatusCode("new");
+        productWarrantyObject.setCreatedDateTime(DateTime.now());
+        return dataAPIClient.post("productwarranty", productWarrantyObject, ProductWarrantyObject.class);
+    }
+}
