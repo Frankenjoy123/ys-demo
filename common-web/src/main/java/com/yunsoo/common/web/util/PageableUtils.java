20dc7305da9b221a973e3196cc4616fcb3884755
Lijian <loye.qiu@gmail.com>
2016-02-23 11:39:03 +0800

    pageable add  totalElements count

--- common-web/src/main/java/com/yunsoo/common/web/util/PageableUtils.java
+++ common-web/src/main/java/com/yunsoo/common/web/util/PageableUtils.java
@@ -13,16 +13,24 @@ public class PageableUtils {
                 (total == null || total < 1 ? "*" : total.toString()));
     }
 
+    public static String formatPages(Integer page, Integer total, Integer count) {
+        return String.format("pages %s/%s/%s",
+                (page == null || page < 0 ? "0" : page.toString()),
+                (total == null || total < 1 ? "*" : total.toString()),
+                (count == null || count < 1 ? "*" : count.toString()));
+    }
+
     public static Integer[] parsePages(String contentRange) {
-        Integer page = 0, total = null;
+        Integer page = 0, total = null, count = null;
         if (contentRange != null) {
             String[] pagesArray = contentRange.replace("pages ", "").split("/");
             try {
                 page = Integer.parseInt(pagesArray[0]);
                 total = "*".equals(pagesArray[1]) ? null : Integer.parseInt(pagesArray[1]);
+                count = pagesArray.length < 3 || "*".equals(pagesArray[2]) ? null : Integer.parseInt(pagesArray[2]);
             } catch (NumberFormatException ignored) {
             }
         }
-        return new Integer[]{page, total};
+        return new Integer[]{page, total, count};
     }
 }
