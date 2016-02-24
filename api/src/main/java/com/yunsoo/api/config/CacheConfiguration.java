89fc6a1a6e7944e39c0223a864e9e47c36147ed9
Lijian <loye.qiu@gmail.com>
2016-02-24 11:36:24 +0800

    fix redis cache issue

--- api/src/main/java/com/yunsoo/api/config/CacheConfiguration.java
+++ api/src/main/java/com/yunsoo/api/config/CacheConfiguration.java
@@ -1,11 +1,14 @@
 package com.yunsoo.api.config;
 
 import com.yunsoo.api.cache.CustomKeyGenerator;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.cloud.aws.cache.config.annotation.CacheClusterConfig;
 import org.springframework.cloud.aws.cache.config.annotation.EnableElastiCache;
+import org.springframework.cloud.aws.context.annotation.ConditionalOnAwsCloudEnvironment;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,8 +21,8 @@ public class CacheConfiguration {
 
     @Configuration
     @EnableElastiCache(value = {@CacheClusterConfig(name = CACHE_NAME)})
-//    @ConditionalOnAwsCloudEnvironment
-//    @ConditionalOnProperty(value = "yunsoo.cache.type", havingValue = "elasticache")
+    @ConditionalOnAwsCloudEnvironment
+    @ConditionalOnProperty(value = "yunsoo.cache.type", havingValue = "elasticache")
     public static class ElastiCacheConfiguration {
 
         @Bean
@@ -29,9 +32,9 @@ public class CacheConfiguration {
 
     }
 
-//    @Configuration
-//    @EnableCaching
-//    @ConditionalOnProperty(value = "yunsoo.cache.type", havingValue = "local")
+    @Configuration
+    @EnableCaching
+    @ConditionalOnProperty(value = "yunsoo.cache.type", havingValue = "local")
     public static class LocalCacheConfiguration {
 
         @Bean
