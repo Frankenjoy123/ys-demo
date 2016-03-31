02dc593d75ff47169c1bc117f8374abccb30c98e
Lijian <loye.qiu@gmail.com>
2016-03-31 14:16:52 +0800

    permission: rename

--- api/src/main/java/com/yunsoo/api/security/permission/expression/RestrictionExpression.java
+++ api/src/main/java/com/yunsoo/api/security/permission/expression/RestrictionExpression.java
@@ -39,15 +39,15 @@ public abstract class RestrictionExpression extends ResourceExpression {
 
     public static class OrgRestrictionExpression extends RestrictionExpression {
 
-        private static final String RESOURCE = "org";
-        private static final String PREFIX = RESOURCE + DELIMITER;
+        private static final String TYPE = "org";
+        private static final String PREFIX = TYPE + DELIMITER;
 
         public static final OrgRestrictionExpression ANY = new OrgRestrictionExpression("*");
         public static final OrgRestrictionExpression CURRENT = new OrgRestrictionExpression("current");
 
         public OrgRestrictionExpression(String expressionOrOrgId) {
             super(expressionOrOrgId);
-            setResource(RESOURCE);
+            setType(TYPE);
         }
 
         public String getOrgId() {
@@ -66,12 +66,12 @@ public abstract class RestrictionExpression extends ResourceExpression {
 
     public static class RegionRestrictionExpression extends RestrictionExpression {
 
-        private static final String RESOURCE = "region";
-        private static final String PREFIX = RESOURCE + DELIMITER;
+        private static final String TYPE = "region";
+        private static final String PREFIX = TYPE + DELIMITER;
 
         public RegionRestrictionExpression(String expressionOrRegionId) {
             super(expressionOrRegionId);
-            setResource(RESOURCE);
+            setType(TYPE);
         }
 
         public String getRegionId() {
@@ -94,7 +94,7 @@ public abstract class RestrictionExpression extends ResourceExpression {
 
         public CollectionRestrictionExpression(String expression) {
             super(expression);
-            setResource(COLLECTION_RESOURCE);
+            setType(COLLECTION_TYPE);
             this.expressions = new ArrayList<>();
             for (String e : expression.split(COLLECTION_DELIMITER)) {
                 RestrictionExpression exp = parse(e);
@@ -104,7 +104,7 @@ public abstract class RestrictionExpression extends ResourceExpression {
 
         public <E extends RestrictionExpression> CollectionRestrictionExpression(List<E> expressions) {
             super(ResourceExpression.toString(expressions));
-            setResource(COLLECTION_RESOURCE);
+            setType(COLLECTION_TYPE);
             if (expressions != null) {
                 this.expressions = expressions.stream().collect(Collectors.toList());
             } else {
