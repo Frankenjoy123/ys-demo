ae5ad79ff314634d3026930e77528078151e1187
seedwht <seedwht@gmail.com>
2016-05-06 15:25:40 +0800

    fix marketing prize issue

--- api-rabbit/src/main/java/com/yunsoo/api/rabbit/controller/MarketingController.java
+++ api-rabbit/src/main/java/com/yunsoo/api/rabbit/controller/MarketingController.java
@@ -98,9 +98,7 @@ public class MarketingController {
             throw new NotFoundException("product can not be found by the key");
         }
         MktDrawPrizeObject mktDrawPrizeObject = mktDrawPrize.toMktDrawPrizeObject();
-        if (mktDrawPrizeObject.getAccountType() == null || mktDrawPrizeObject.getAccountType() == "") {
-            mktDrawPrizeObject.setAccountType("webchat");
-        }
+
         MktDrawPrizeObject newMktDrawPrizeObject = marketingDomain.createMktDrawPrize(mktDrawPrizeObject);
         return new MktDrawPrize(newMktDrawPrizeObject);
     }
@@ -111,9 +109,7 @@ public class MarketingController {
             throw new BadRequestException("marketing draw record can not be null");
         }
         MktDrawPrizeObject mktDrawPrizeObject = mktDrawPrize.toMktDrawPrizeObject();
-        if (mktDrawPrizeObject.getAccountType() == null || mktDrawPrizeObject.getAccountType() == "") {
-            mktDrawPrizeObject.setAccountType("webchat");
-        }
+
         mktDrawPrizeObject.setStatusCode(LookupCodes.MktDrawPrizeStatus.SUBMIT);
         marketingDomain.updateMktDrawPrize(mktDrawPrizeObject);
     }
