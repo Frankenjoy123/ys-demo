675ca0ccabd9bba5c2a635c9be388754a96df3da
seedwht <seedwht@gmail.com>
2016-07-04 21:09:24 +0800

    Fix small issue

--- api/src/main/java/com/yunsoo/api/dto/MarketWinUserLocationAnalysis.java
+++ api/src/main/java/com/yunsoo/api/dto/MarketWinUserLocationAnalysis.java
@@ -3,6 +3,9 @@ package com.yunsoo.api.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.yunsoo.common.data.object.MarketWinUserLocationAnalysisObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by:  haitao
  * Created on:  2016/6/1
@@ -14,6 +17,9 @@ public class MarketWinUserLocationAnalysis {
 
     @JsonProperty("province")
     private String province;
+
+    @JsonProperty("city")
+    private List<MarketWinUserLocationAnalysis> city;
 
     public int getCount() {
         return count;
@@ -38,7 +44,28 @@ public class MarketWinUserLocationAnalysis {
         if (object != null) {
             this.setCount(object.getValue());
             this.setProvince(object.getName());
+            List<MarketWinUserLocationAnalysis> cityData = new ArrayList<MarketWinUserLocationAnalysis>();
+            if (object.getCityData().size() > 0) {
+                for (MarketWinUserLocationAnalysisObject temp : object.getCityData()) {
+                    int cityCount = temp.getValue();
+                    String cityName = temp.getName();
+                    MarketWinUserLocationAnalysis tempList = new MarketWinUserLocationAnalysis();
+                    tempList.setProvince(cityName);
+                    tempList.setCount(cityCount);
+                    cityData.add(tempList);
+                }
+            }
+            this.setCity(cityData);
         }
+
+
     }
 
+    public List<MarketWinUserLocationAnalysis> getCity() {
+        return city;
+    }
+
+    public void setCity(List<MarketWinUserLocationAnalysis> city) {
+        this.city = city;
+    }
 }
