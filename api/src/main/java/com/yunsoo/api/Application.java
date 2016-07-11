1d4cafa7d4ac5d9d57cb548030ed216436f9d005
yan <yan@yunsu.com>
2016-07-11 19:47:28 +0800

    change the get property method to utils

--- api/src/main/java/com/yunsoo/api/Application.java
+++ api/src/main/java/com/yunsoo/api/Application.java
@@ -1,12 +1,16 @@
 package com.yunsoo.api;
 
 import com.yunsoo.api.thread.OperationThread;
+import com.yunsoo.api.util.PropertyUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+
+
+import java.util.Properties;
 
 @ComponentScan(basePackages = "com.yunsoo")
 @SpringBootApplication
@@ -15,13 +19,18 @@ public class Application {
     private static Log log = LogFactory.getLog(Application.class);
 
     public static void main(String[] args) {
+
+
         ConfigurableApplicationContext app = SpringApplication.run(Application.class, args);
         //app.getClassLoader();
         log.info(String.format("%s started...", app.getId()));
 
-        OperationThread operationThread = new OperationThread("http://localhost:3080/");
+        OperationThread operationThread = new OperationThread(PropertyUtils.getProperty("yunsoo.client.dataapi.baseurl"));
         operationThread.run();
 
     }
+
+
+
 
 }
