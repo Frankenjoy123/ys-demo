ac26726adb2336ddea8933c9ebc6c9a2c17c171a
yan <yan@yunsu.com>
2016-07-27 12:32:58 +0800

    do changes for api of marketing

--- api/src/main/java/com/yunsoo/api/controller/TaskFileController.java
+++ api/src/main/java/com/yunsoo/api/controller/TaskFileController.java
@@ -102,9 +102,9 @@ public class TaskFileController {
 
     @RequestMapping(value = "count", method = RequestMethod.GET)
     public long[] getTotalTaskFiles(@RequestParam(value = "device_id", required = false) String deviceId,
-                                   @RequestParam(value = "type_code") String typeCode,
-                                  @RequestParam(value = "created_datetime_start", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) DateTime start,
-                                  @RequestParam(value = "created_datetime_end", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) DateTime end){
+                                    @RequestParam(value = "type_code") String typeCode,
+                                    @RequestParam(value = "created_datetime_start", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) DateTime start,
+                                    @RequestParam(value = "created_datetime_end", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) DateTime end) {
 
         String orgId = AuthUtils.fixOrgId(null);
         return new long[]{1, 1};
