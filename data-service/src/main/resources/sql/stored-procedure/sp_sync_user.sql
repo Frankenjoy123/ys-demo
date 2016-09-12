30a6237a731468b88a48c536b734dda63e578493
pagepinner <yqy0902@gmail.com>
2016-09-12 18:21:46 +0800

    数据更新

--- data-service/src/main/resources/sql/stored-procedure/sp_sync_user.sql
+++ data-service/src/main/resources/sql/stored-procedure/sp_sync_user.sql
@@ -30,13 +30,13 @@ BEGIN
       `last_event_ip` varchar(55) DEFAULT NULL COMMENT 'ip',
       `last_user_agent` varchar(255) DEFAULT NULL COMMENT '设备'
     );
-
+    
 	create temporary table sp_sync_user_tmp_user_phone
     (
       `user_id` char(19) DEFAULT NULL COMMENT '用户id',
       `ys_id` varchar(32) DEFAULT NULL COMMENT 'ysid，当匿名访问时，ysid作为唯一区分用户的id',
       `org_id` char(19) DEFAULT NULL COMMENT '品牌商id',
-      `phone` nvarchar(50) DEFAULT NULL COMMENT '手机号码'
+      `phone` nvarchar(50) DEFAULT NULL COMMENT '手机号码'      
     );
 
 
@@ -59,36 +59,37 @@ BEGIN
         usr2.ip,usr2.user_agent
       from  user u
         inner join (
-                     SELECT usr.user_id, pb.org_id, min(usr.created_datetime) as 'created_datetime',usr.id, province, city, address, usr.ip, usr.user_agent
+                     SELECT usr.user_id, pb.org_id, min(usr.created_datetime) as 'created_datetime', max(usr.created_datetime) as 'last_event_time', usr.id, province, city, address, usr.ip, usr.user_agent
                      FROM yunsoo2015DB.user_scan_record usr inner join product_base pb
                          on usr.product_base_id = pb.id group by usr.user_id, pb.org_id order by usr.id desc) as usr2 on u.id = usr2.user_id
-
+        
       where (u.created_datetime >= min_value_date AND u.created_datetime < max_value_date)
-            or(u.modified_datetime >= min_value_date AND u.modified_datetime < max_value_date);
+            or(u.modified_datetime >= min_value_date AND u.modified_datetime < max_value_date)
+            or (usr2.last_event_time >= min_value_date AND usr2.last_event_time < max_value_date);
 
     -- where (u.modified_datetime is null and u.created_datetime >= @max_value_date) or u.modified_datetime >= @max_value_date;
-
+    
     -- 插入ysId不为空，而user——id为空或者固定的。用来读取匿名用户
     insert into sp_sync_user_tmp_user
       SELECT '', usr.ysid, pb.org_id as 'org_id', '' as 'org_name', null, null, null, null, null, null, null,
         usr.province, usr.city, usr.address, usr.created_datetime,usr.ip, usr.user_agent
       from user_scan_record usr inner join product_base pb
-          on usr.product_base_id = pb.id
+          on usr.product_base_id = pb.id     
       where usr.created_datetime >= min_value_date and usr.created_datetime < max_value_date
             and (usr.user_id is null or usr.user_id = '0020000000000000000')
       group by usr.ysid, pb.org_id order by usr.id desc;
 
     -- select * from sp_sync_user_tmp_user;
-
+    
     -- select * from sp_sync_user_tmp_user;
-    insert into sp_sync_user_tmp_user_phone
+    insert into sp_sync_user_tmp_user_phone 
    select up.user_id, up.ys_id, up.org_id, mdp.mobile
-    FROM mkt_draw_prize mdp inner join
+    FROM mkt_draw_prize mdp inner join 
    (select case when usr.user_id is null or usr.user_id = '0020000000000000000' then '' else usr.user_id end as 'user_id',
 case when usr.user_id is not null and usr.user_id <> '0020000000000000000' then '' else usr.ysid end as 'ys_id'
 , pb.org_id, max(mdp2.draw_record_id) as 'last_draw_prize_id' FROM user_scan_record usr inner join product_base pb
           on usr.product_base_id = pb.id
-        inner join mkt_draw_prize mdp2 on mdp2.scan_record_id = usr.id
+        inner join mkt_draw_prize mdp2 on mdp2.scan_record_id = usr.id 
         where mdp2.status_code in('paid','submit') and mdp2.mobile is not null group by user_id, ys_id, pb.org_id)
         as up on mdp.draw_record_id = up.last_draw_prize_id
         where mdp.paid_datetime >= min_value_date and mdp.paid_datetime < max_value_date;
@@ -96,14 +97,14 @@ case when usr.user_id is not null and usr.user_id <> '0020000000000000000' then 
     start transaction;
 
     -- 先更新部分数据
-  update emr_user u
+  update emr_user u 
       inner join sp_sync_user_tmp_user tmp
         on u.org_id = tmp.org_id and (u.user_id = tmp.user_id and u.ys_id = tmp.ys_id)
            left join lu_province_city l on (l.city = tmp.city  or l.city = concat(tmp.city,'市') )
         set u.email = tmp.email, u.age = tmp.age, u.name = tmp.name, u.sex = tmp.sex, u.gravatar_url = tmp.gravatar_url,
         u.wx_openid = tmp.wx_openid, u.province = ifnull(l.province,tmp.province),
         u.city =  ifnull(l.city,tmp.city);
-
+  
 
 
     -- 插入user——id
@@ -111,7 +112,7 @@ case when usr.user_id is not null and usr.user_id <> '0020000000000000000' then 
       select usr.user_id, usr.ys_id, usr.org_id, usr.org_name, usr.name, usr.phone, usr.email,usr.age,
         usr.sex, usr.gravatar_url, usr.wx_openid, ifnull(l.province,usr.province), ifnull(l.city,usr.city),
         usr.address,usr.join_datetime, usr.last_event_ip,
-        case when usr.last_user_agent like '%iphone%' then 'iPhone'
+        case when usr.last_user_agent like '%iphone%' or usr.last_user_agent like '%ipad%' then 'iPhone'
         when usr.last_user_agent like '%android%' then 'Android'
         when usr.last_user_agent like '%windows%' then 'Windows'
         else '其他' end
