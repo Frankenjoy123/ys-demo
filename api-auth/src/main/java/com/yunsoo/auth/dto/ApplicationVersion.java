12f4ce1b9a0dac2a151c309206555e0c6fe79132
Frankenjoy123 <frankenjoy123@gmail.com>
2016-11-29 14:38:07 +0800

    [api-auth]: add application/{appId}/version api

---
+++ api-auth/src/main/java/com/yunsoo/auth/dto/ApplicationVersion.java
@@ -0,0 +1,101 @@
+package com.yunsoo.auth.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.yunsoo.common.databind.DateTimeJsonDeserializer;
+import com.yunsoo.common.databind.DateTimeJsonSerializer;
+import org.hibernate.annotations.Type;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.joda.time.DateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+/**
+ * Created by:   Xiaowu
+ * Created on:   2016/11/29
+ * Descriptions:
+ */
+public class ApplicationVersion implements Serializable {
+
+    @JsonProperty("id")
+    private int id;
+
+    @JsonProperty("app_id")
+    private String appId;
+
+    @JsonProperty("version_code")
+    private int versionCode;
+
+    @JsonSerialize(using = DateTimeJsonSerializer.class)
+    @JsonDeserialize(using = DateTimeJsonDeserializer.class)
+    @JsonProperty("created_datetime")
+    private DateTime createdDateTime;
+
+    @JsonProperty("download_url")
+    private String downloadUrl;
+
+    @JsonProperty("comments")
+    private String comments;
+
+    @JsonProperty("status_code")
+    private String statusCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAppId() {
+        return appId;
+    }
+
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
+
+    public int getVersionCode() {
+        return versionCode;
+    }
+
+    public void setVersionCode(int versionCode) {
+        this.versionCode = versionCode;
+    }
+
+    public DateTime getCreatedDateTime() {
+        return createdDateTime;
+    }
+
+    public void setCreatedDateTime(DateTime createdDateTime) {
+        this.createdDateTime = createdDateTime;
+    }
+
+    public String getDownloadUrl() {
+        return downloadUrl;
+    }
+
+    public void setDownloadUrl(String downloadUrl) {
+        this.downloadUrl = downloadUrl;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public String getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(String statusCode) {
+        this.statusCode = statusCode;
+    }
+}
