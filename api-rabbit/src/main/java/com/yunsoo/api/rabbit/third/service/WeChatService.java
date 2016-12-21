f52af10bb348f7dd9750a29c0b71db86905cc76d
Lijian <loye.qiu@gmail.com>
2016-12-21 14:49:28 +0800

    fix warning

--- api-rabbit/src/main/java/com/yunsoo/api/rabbit/third/service/WeChatService.java
+++ api-rabbit/src/main/java/com/yunsoo/api/rabbit/third/service/WeChatService.java
@@ -4,12 +4,15 @@ import com.yunsoo.api.rabbit.client.ThirdApiClient;
 import com.yunsoo.api.rabbit.third.dto.WeChatAccessToken;
 import com.yunsoo.common.web.util.QueryStringBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
 /**
- * Created by yan on 12/7/2016.
+ * Created by:   yan
+ * Created on:   12/7/2016
+ * Descriptions:
  */
 @Service
 public class WeChatService {
@@ -29,7 +32,8 @@ public class WeChatService {
         String query = new QueryStringBuilder(QueryStringBuilder.Prefix.QUESTION_MARK)
                 .append("app_id", appId).append("url", url)
                 .build();
-        return  thirdApiClient.get("wechat/jssdk/config" + query, Map.class);
+        return thirdApiClient.get("wechat/jssdk/config" + query, new ParameterizedTypeReference<Map<String, Object>>() {
+        });
     }
 
 
