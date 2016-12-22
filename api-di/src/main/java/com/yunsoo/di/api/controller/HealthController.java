d7b5f5050d74299805bcf3192fe4933d2659662c
Lijian <loye.qiu@gmail.com>
2016-12-22 15:41:01 +0800

    diApiClient health check

---
+++ api-di/src/main/java/com/yunsoo/di/api/controller/HealthController.java
@@ -0,0 +1,15 @@
+package com.yunsoo.di.api.controller;
+
+import com.yunsoo.common.web.health.AbstractHealthController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by:   Lijian
+ * Created on:   2016-08-15
+ * Descriptions:
+ */
+@RestController
+@RequestMapping(value = "/health")
+public class HealthController extends AbstractHealthController {
+}
